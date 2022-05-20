9e51a5749da4c88f507eeaad26db51425d172c3f
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2022-05-20 17:54:30 -0400

    More trimming on $this->load->model

--- upload/admin/controller/extension/payment/pilibaba.php
+++ upload/admin/controller/extension/payment/pilibaba.php
@@ -4,7 +4,6 @@ class ControllerExtensionPaymentPilibaba extends Controller {
 
 	public function index() {
 		$this->load->model('setting/setting');
-
 		$this->load->model('extension/payment/pilibaba');
 
 		$this->load->language('extension/payment/pilibaba');
