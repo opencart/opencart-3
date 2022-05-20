12ad0e4139a27af5923d909b60b03810422b5355
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2022-05-20 18:07:14 -0400

    More trimming on $this->load->model

--- upload/catalog/controller/extension/payment/laybuy.php
+++ upload/catalog/controller/extension/payment/laybuy.php
@@ -4,7 +4,6 @@ class ControllerExtensionPaymentLaybuy extends Controller {
 		$this->load->language('extension/payment/laybuy');
 
 		$this->load->model('extension/payment/laybuy');
-
 		$this->load->model('checkout/order');
 
 		if(!isset($this->session->data['order_id'])) {
