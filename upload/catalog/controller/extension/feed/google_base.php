85bbe119f6b8282901a3932af61ce33d0ab5c2fb
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2022-05-20 17:59:14 -0400

    More trimming on $this->load->model

--- upload/catalog/controller/extension/feed/google_base.php
+++ upload/catalog/controller/extension/feed/google_base.php
@@ -12,7 +12,6 @@ class ControllerExtensionFeedGoogleBase extends Controller {
 			$this->load->model('extension/feed/google_base');
 			$this->load->model('catalog/category');
 			$this->load->model('catalog/product');
-
 			$this->load->model('tool/image');
 
 			$product_data = array();
