903143e9705a13a2c6591bf604af2ece72ae8b49
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2022-05-20 18:09:40 -0400

    More trimming on $this->load->model

--- upload/catalog/controller/product/search.php
+++ upload/catalog/controller/product/search.php
@@ -4,9 +4,7 @@ class ControllerProductSearch extends Controller {
 		$this->load->language('product/search');
 
 		$this->load->model('catalog/category');
-
 		$this->load->model('catalog/product');
-
 		$this->load->model('tool/image');
 
 		if (isset($this->request->get['search'])) {
