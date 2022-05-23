4766f64be8390f20cac5c7f9dd2a5e7b500f7581
TheCartpenter <scoutch@gmail.com>
2022-05-23 15:43:46 -0400

    Added comments on language definitions.

--- upload/catalog/language/en-gb/extension/payment/securetrading_pp.php
+++ upload/catalog/language/en-gb/extension/payment/securetrading_pp.php
@@ -1,8 +1,9 @@
 <?php
-
-$_['text_title'] = 'Credit / Debit card';
+// Button
 $_['button_confirm'] = 'Confirm';
 
+// Text
+$_['text_title'] = 'Credit / Debit card';
 $_['text_postcode_check'] = 'Postcode check: %s';
 $_['text_security_code_check'] = 'CVV2 check: %s';
 $_['text_address_check'] = 'Address check: %s';
@@ -12,4 +13,5 @@ $_['text_match'] = 'Matched';
 $_['text_not_match'] = 'Not matched';
 $_['text_payment_details'] = 'Payment details';
 
+// Entry
 $_['entry_card_type'] = 'Card type';
