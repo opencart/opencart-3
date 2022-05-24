d58bd2a80ce578003f89ac5cbcbee0c4e2a90eb7
TheCartpenter <scoutch@gmail.com>
2022-05-24 17:38:04 -0400

    Pushed more load language.

--- upload/admin/controller/extension/module/divido_calculator.php
+++ upload/admin/controller/extension/module/divido_calculator.php
@@ -4,13 +4,16 @@ class ControllerExtensionModuleDividoCalculator extends Controller {
 
 	public function index() {
 		$this->load->language('extension/module/divido_calculator');
+		
 		$this->load->model('setting/setting');
 
 		$this->document->setTitle($this->language->get('heading_title'));
 
 		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
 			$this->model_setting_setting->editSetting('module_divido_calculator', $this->request->post);
+			
 			$this->session->data['success'] = $this->language->get('text_success');
+			
 			$this->response->redirect($this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true));
 		}
 
