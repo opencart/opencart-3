0522a0133ed45f4f5eaec6c3f9d112271fd9d4ab
TheCartpenter <scoutch@gmail.com>
2022-05-25 18:37:48 -0400

    Renamed all return to returns.

--- upload/admin/language/en-gb/mail/returns.php
+++ upload/admin/language/en-gb/mail/returns.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 // Text
 $_['text_subject']       = '%s - Return Update %s';
@@ -5,4 +6,13 @@ $_['text_return_id']     = 'Return ID:';
 $_['text_date_added']    = 'Return Date:';
 $_['text_return_status'] = 'Your return has been updated to the following status:';
 $_['text_comment']       = 'The comments for your return are:';
+=======
+<?php
+// Text
+$_['text_subject']       = '%s - Return Update %s';
+$_['text_return_id']     = 'Return ID:';
+$_['text_date_added']    = 'Return Date:';
+$_['text_return_status'] = 'Your return has been updated to the following status:';
+$_['text_comment']       = 'The comments for your return are:';
+>>>>>>> 55b2709 (Renamed all return to returns.)
 $_['text_footer']        = 'Please reply to this email if you have any questions.';
