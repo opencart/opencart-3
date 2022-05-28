ebedcc970101043d591835504aab463dd8f4eaf9
TheCartpenter <scoutch@gmail.com>
2022-05-28 18:41:18 -0400

    Reverted changes for install/controller/startup.php file.

--- upload/install/controller/startup/router.php
+++ upload/install/controller/startup/router.php
@@ -2,14 +2,9 @@
 class ControllerStartupRouter extends Controller {
 	public function index() {
 		if (isset($this->request->get['route']) && $this->request->get['route'] != 'action/route') {
-			$route = $this->request->get['route'];
+			return new \Action($this->request->get['route']);
 		} else {
-			$route = $this->config->get('action_default');
+			return new \Action($this->config->get('action_default'));
 		}
-		
-		// Sanitize the call
-		$route = preg_replace('/[^a-zA-Z0-9_\/]/', '', (string)$route);
-		
-		return new \Action($route);
 	}
 }
