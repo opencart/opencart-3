647cf993db1a389af110a276865dcfa67e7b46fe
TheCartpenter <scoutch@gmail.com>
2022-05-28 18:38:00 -0400

    Edited install/controller/startup/router.php to match the admin and catalog startup router controller lookup.

--- upload/install/controller/startup/router.php
+++ upload/install/controller/startup/router.php
@@ -2,9 +2,14 @@
 class ControllerStartupRouter extends Controller {
 	public function index() {
 		if (isset($this->request->get['route']) && $this->request->get['route'] != 'action/route') {
-			return new \Action($this->request->get['route']);
+			$route = $this->request->get['route'];			
 		} else {
-			return new \Action($this->config->get('action_default'));
+			$route = $this->config->get('action_default');
 		}
+		
+		// Sanitize the call
+		$route = preg_replace('/[^a-zA-Z0-9_\/]/', '', (string)$route);
+		
+		return new \Action($route);
 	}
 }
