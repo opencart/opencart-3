1c415b6a39daf3686a4b797871e51d81babac98c
TheCartpenter <scoutch@gmail.com>
2022-05-29 17:17:23 -0400

    Aligned more keys and values array.

--- upload/catalog/model/extension/payment/bluepay_redirect.php
+++ upload/catalog/model/extension/payment/bluepay_redirect.php
@@ -19,10 +19,10 @@ class ModelExtensionPaymentBluePayRedirect extends Model {
 
 		if ($status) {
 			$method_data = array(
-				'code' => 'bluepay_redirect',
-				'title' => $this->language->get('text_title'),
-				'terms' => '',
-				'sort_order' => $this->config->get('payment_bluepay_redirect_sort_order')
+				'code' 			=> 'bluepay_redirect',
+				'title' 		=> $this->language->get('text_title'),
+				'terms' 		=> '',
+				'sort_order' 	=> $this->config->get('payment_bluepay_redirect_sort_order')
 			);
 		}
 
@@ -38,12 +38,12 @@ class ModelExtensionPaymentBluePayRedirect extends Model {
 
 		foreach ($query->rows as $row) {
 			$card_data[] = array(
-				'card_id' => $row['card_id'],
-				'customer_id' => $row['customer_id'],
-				'token' => $row['token'],
-				'digits' => '**** ' . $row['digits'],
-				'expiry' => $row['expiry'],
-				'type' => $row['type'],
+				'card_id' 		=> $row['card_id'],
+				'customer_id' 	=> $row['customer_id'],
+				'token' 		=> $row['token'],
+				'digits' 		=> '**** ' . $row['digits'],
+				'expiry' 		=> $row['expiry'],
+				'type' 			=> $row['type'],
 			);
 		}
 		
