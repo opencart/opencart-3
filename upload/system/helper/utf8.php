a486a01cf4fe3208b2ec7f7951778bed0c13cc1b
TheCartpenter <scoutch@gmail.com>
2022-06-07 12:29:50 -0400

    Remove variable types from utf-8 helper.

--- upload/system/helper/utf8.php
+++ upload/system/helper/utf8.php
@@ -2,44 +2,44 @@
 if (extension_loaded('mbstring')) {
 	mb_internal_encoding('UTF-8');
 
-	function utf8_strlen(string $string) {
+	function utf8_strlen($string) {
 		return mb_strlen($string);
 	}
 
-	function utf8_strpos(string $string, string $needle, int $offset = 0) {
+	function utf8_strpos($string, $needle, $offset = 0) {
 		return mb_strpos($string, $needle, $offset);
 	}
 
-	function utf8_strrpos(string $string, string $needle, int $offset = 0) {
+	function utf8_strrpos($string, $needle, $offset = 0) {
 		return mb_strrpos($string, $needle, $offset);
 	}
 
-	function utf8_substr(string $string, int $offset, ?int $length = null) {
+	function utf8_substr($string, $offset, $length = null) {
 		return mb_substr($string, $offset, $length);
 	}
 
-	function utf8_strtoupper(string $string) {
+	function utf8_strtoupper($string) {
 		return mb_strtoupper($string);
 	}
 
-	function utf8_strtolower(string $string) {
+	function utf8_strtolower($string) {
 		return mb_strtolower($string);
 	}
 
 } elseif (function_exists('iconv')) {
-	function utf8_strlen(string $string) {
+	function utf8_strlen($string) {
 		return iconv_strlen($string, 'UTF-8');
 	}
 
-	function utf8_strpos(string $string, string $needle, int $offset = 0) {
+	function utf8_strpos($string, $needle, $offset = 0) {
 		return iconv_strpos($string, $needle, $offset, 'UTF-8');
 	}
 
-	function utf8_strrpos(string $string, string $needle) {
+	function utf8_strrpos($string, $needle) {
 		return iconv_strrpos($string, $needle, 'UTF-8');
 	}
 
-	function utf8_substr(string $string, int $offset, ?int $length = null) {
+	function utf8_substr($string, $offset, $length = null) {
 		return iconv_substr($string, $offset, $length, 'UTF-8');
 	}
 
