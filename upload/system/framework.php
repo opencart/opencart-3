ad3f3dbf25d1854412e25c3716aaa0f97ef126b7
TheCartpenter <scoutch@gmail.com>
2022-06-09 19:40:30 -0400

    Finished session_expire cookie parameters.

--- upload/system/framework.php
+++ upload/system/framework.php
@@ -109,7 +109,7 @@ if ($config->get('session_autostart')) {
 
 	$session->start($session_id);
 
-	setcookie($config->get('session_name'), $session->getId(), ini_get('session.cookie_lifetime'), ini_get('session.cookie_path'), ini_get('session.cookie_domain'));
+	setcookie($config->get('session_name'), $session->getId(), $config->get('config_session_expire'), ini_get('session.cookie_path'), ini_get('session.cookie_domain'));
 }
 
 // Cache
