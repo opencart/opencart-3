47b26c8ff291ddc22d17c3b2cd843f8540dd9a65
TheCartpenter <scoutch@gmail.com>
2022-06-09 20:30:16 -0400

    Modified login() method in customer and user library.

--- upload/system/library/cart/user.php
+++ upload/system/library/cart/user.php
@@ -1,10 +1,10 @@
 <?php
 namespace Cart;
 class User {
-	private $user_id;
-	private $user_group_id;
-	private $username;
-	private $permission = array();
+	private $user_id = 0;
+	private $username = '';
+	private $user_group_id = 0;
+	private $permission = [];
 
 	public function __construct($registry) {
 		$this->db = $registry->get('db');
@@ -37,9 +37,23 @@ class User {
 	}
 
 	public function login($username, $password) {
-		$user_query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "user` WHERE `username` = '" . $this->db->escape($username) . "' AND (`password` = SHA1(CONCAT(`salt`, SHA1(CONCAT(`salt`, SHA1('" . $this->db->escape($password) . "'))))) OR `password` = '" . $this->db->escape(md5($password)) . "') AND `status` = '1'");
+		$user_query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "user` WHERE `username` = '" . $this->db->escape($username) . "' AND `status` = '1'");
 
 		if ($user_query->num_rows) {
+			if (password_verify($password, $user_query->row['password'])) {
+				$rehash = password_needs_rehash($user_query->row['password'], PASSWORD_DEFAULT);
+			} elseif (isset($user_query->row['salt']) && $user_query->row['password'] == sha1($user_query->row['salt'] . sha1($user_query->row['salt'] . sha1($password)))) {
+				$rehash = true;
+			} elseif ($user_query->row['password'] == md5($password)) {
+				$rehash = true;
+			} else {
+				return false;
+			}
+
+			if ($rehash) {
+				$this->db->query("UPDATE `" . DB_PREFIX . "user` SET `password` = '" . $this->db->escape(password_hash($password, PASSWORD_DEFAULT)) . "' WHERE `user_id` = '" . (int)$user_query->row['user_id'] . "'");
+			}
+
 			$this->session->data['user_id'] = $user_query->row['user_id'];
 
 			$this->user_id = $user_query->row['user_id'];
@@ -65,7 +79,7 @@ class User {
 	public function logout() {
 		unset($this->session->data['user_id']);
 
-		$this->user_id = '';
+		$this->user_id = 0;
 		$this->username = '';
 	}
 
@@ -78,7 +92,7 @@ class User {
 	}
 
 	public function isLogged() {
-		return $this->user_id;
+		return ($this->user_id ? true : false);
 	}
 
 	public function getId() {
