aec8a7b93388551dfd176c6ee9d473c600d2d8c4
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2022-06-11 14:12:51 -0400

    Restored customer library file

--- upload/system/library/cart/customer.php
+++ upload/system/library/cart/customer.php
@@ -1,13 +1,14 @@
 <?php
 namespace Cart;
 class Customer {
-	private $customer_id = 0;
-	private $firstname = '';
-	private $lastname = '';
-	private $customer_group_id = 0;
-	private $email = '';
-	private $telephone = '';
-	private $newsletter = false;
+	private $customer_id;
+	private $firstname;
+	private $lastname;
+	private $customer_group_id;
+	private $email;
+	private $telephone;
+	private $newsletter;
+	private $address_id;
 
 	public function __construct($registry) {
 		$this->config = $registry->get('config');
@@ -26,8 +27,15 @@ class Customer {
 				$this->email = $customer_query->row['email'];
 				$this->telephone = $customer_query->row['telephone'];
 				$this->newsletter = $customer_query->row['newsletter'];
+				$this->address_id = $customer_query->row['address_id'];
 
 				$this->db->query("UPDATE `" . DB_PREFIX . "customer` SET `language_id` = '" . (int)$this->config->get('config_language_id') . "', `ip` = '" . $this->db->escape($this->request->server['REMOTE_ADDR']) . "' WHERE `customer_id` = '" . (int)$this->customer_id . "'");
+
+				$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "customer_ip` WHERE `customer_id` = '" . (int)$this->session->data['customer_id'] . "' AND `ip` = '" . $this->db->escape($this->request->server['REMOTE_ADDR']) . "'");
+
+				if (!$query->num_rows) {
+					$this->db->query("INSERT INTO `" . DB_PREFIX . "customer_ip` SET `customer_id` = '" . (int)$this->session->data['customer_id'] . "', `ip` = '" . $this->db->escape($this->request->server['REMOTE_ADDR']) . "', `date_added` = NOW()");
+				}
 			} else {
 				$this->logout();
 			}
@@ -35,25 +43,13 @@ class Customer {
 	}
 
   public function login($email, $password, $override = false) {
-		$customer_query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "customer` WHERE LOWER(`email`) = '" . $this->db->escape(utf8_strtolower($email)) . "' AND `status` = '1'");
+		if ($override) {
+			$customer_query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "customer` WHERE LOWER(`email`) = '" . $this->db->escape(utf8_strtolower($email)) . "' AND `status` = '1'");
+		} else {
+			$customer_query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "customer` WHERE LOWER(`email`) = '" . $this->db->escape(utf8_strtolower($email)) . "' AND (`password` = SHA1(CONCAT(`salt`, SHA1(CONCAT(`salt`, SHA1('" . $this->db->escape($password) . "'))))) OR `password` = '" . $this->db->escape(md5($password)) . "') AND `status` = '1'");
+		}
 
-		if ($customer_query->row) {
-			if (!$override) {
-				if (password_verify($password, $customer_query->row['password'])) {
-					$rehash = password_needs_rehash($customer_query->row['password'], PASSWORD_DEFAULT);
-				} elseif (isset($customer_query->row['salt']) && $customer_query->row['password'] == sha1($customer_query->row['salt'] . sha1($customer_query->row['salt'] . sha1($password)))) {
-					$rehash = true;
-				} elseif ($customer_query->row['password'] == md5($password)) {
-					$rehash = true;
-				} else {
-					return false;
-				}
-
-				if ($rehash) {
-					$this->db->query("UPDATE `" . DB_PREFIX . "customer` SET `password` = '" . $this->db->escape(password_hash($password, PASSWORD_DEFAULT)) . "' WHERE `customer_id` = '" . (int)$customer_query->row['customer_id'] . "'");
-				}
-			}
-
+		if ($customer_query->num_rows) {
 			$this->session->data['customer_id'] = $customer_query->row['customer_id'];
 
 			$this->customer_id = $customer_query->row['customer_id'];
@@ -63,7 +59,8 @@ class Customer {
 			$this->email = $customer_query->row['email'];
 			$this->telephone = $customer_query->row['telephone'];
 			$this->newsletter = $customer_query->row['newsletter'];
-
+			$this->address_id = $customer_query->row['address_id'];
+		
 			$this->db->query("UPDATE `" . DB_PREFIX . "customer` SET `language_id` = '" . (int)$this->config->get('config_language_id') . "', `ip` = '" . $this->db->escape($this->request->server['REMOTE_ADDR']) . "' WHERE `customer_id` = '" . (int)$this->customer_id . "'");
 
 			return true;
@@ -75,13 +72,14 @@ class Customer {
 	public function logout() {
 		unset($this->session->data['customer_id']);
 
-		$this->customer_id = 0;
+		$this->customer_id = '';
 		$this->firstname = '';
 		$this->lastname = '';
-		$this->customer_group_id = 0;
+		$this->customer_group_id = '';
 		$this->email = '';
 		$this->telephone = '';
-		$this->newsletter = false;
+		$this->newsletter = '';
+		$this->address_id = '';
 	}
 
 	public function isLogged() {
@@ -117,24 +115,18 @@ class Customer {
 	}
 
 	public function getAddressId() {
-		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "address` WHERE `customer_id` = '" . (int)$this->customer_id . "' AND `default` = '1'");
-
-		if ($query->num_rows) {
-			return (int)$query->row['address_id'];
-		} else {
-			return 0;
-		}
+		return $this->address_id;
 	}
 
 	public function getBalance() {
-		$query = $this->db->query("SELECT SUM(`amount`) AS `total` FROM `" . DB_PREFIX . "customer_transaction` WHERE `customer_id` = '" . (int)$this->customer_id . "'");
+		$query = $this->db->query("SELECT SUM(`amount`) AS total FROM `" . DB_PREFIX . "customer_transaction` WHERE `customer_id` = '" . (int)$this->customer_id . "'");
 
-		return (float)$query->row['total'];
+		return (int)$query->row['total'];
 	}
 
 	public function getRewardPoints() {
-		$query = $this->db->query("SELECT SUM(`points`) AS `total` FROM `" . DB_PREFIX . "customer_reward` WHERE `customer_id` = '" . (int)$this->customer_id . "'");
+		$query = $this->db->query("SELECT SUM(`points`) AS total FROM `" . DB_PREFIX . "customer_reward` WHERE `customer_id` = '" . (int)$this->customer_id . "'");
 
-		return (float)$query->row['total'];
+		return (int)$query->row['total'];
 	}
 }
