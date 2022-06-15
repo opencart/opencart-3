3681af9f80a1a8fbe947793dfa29bb2a1d78b283
TheCartpenter <scoutch@gmail.com>
2022-06-15 14:08:45 -0400

    Added new files for the controller/startup in the admin.

---
+++ upload/admin/controller/startup/application.php
@@ -0,0 +1,35 @@
+<?php
+class ControllerStartupApplication extends Controller {
+	public function index() {
+		// Url
+		$this->registry->set('url', new \Url($this->config->get('site_url')));
+
+		// Customer
+		$this->registry->set('customer', new \Cart\Customer($this->registry));
+
+		// Currency
+		$this->registry->set('currency', new \Cart\Currency($this->registry));
+
+		// Tax
+		$this->registry->set('tax', new \Cart\Tax($this->registry));
+
+		if ($this->config->get('config_tax_default') == 'shipping') {
+			$this->tax->setShippingAddress((int)$this->config->get('config_country_id'), (int)$this->config->get('config_zone_id'));
+		}
+
+		if ($this->config->get('config_tax_default') == 'payment') {
+			$this->tax->setPaymentAddress((int)$this->config->get('config_country_id'), (int)$this->config->get('config_zone_id'));
+		}
+
+		$this->tax->setStoreAddress((int)$this->config->get('config_country_id'), (int)$this->config->get('config_zone_id'));
+
+		// Weight
+		$this->registry->set('weight', new \Cart\Weight($this->registry));
+		
+		// Length
+		$this->registry->set('length', new \Cart\Length($this->registry));
+		
+		// Cart
+		$this->registry->set('cart', new \Cart\Cart($this->registry));
+	}
+}
---
+++ upload/admin/controller/startup/setting.php
@@ -0,0 +1,28 @@
+<?php
+class ControllerStartupSetting extends Controller {
+	public function index() {
+		$this->load->model('setting/setting');
+
+		// Settings
+		$results = $this->model_setting_setting->getSettings(0);
+
+		foreach ($results as $result) {
+			if (!$result['serialized']) {
+				$this->config->set($result['key'], $result['value']);
+			} else {
+				$this->config->set($result['key'], json_decode($result['value'], true));
+			}
+		}
+
+		// Set time zone
+		if ($this->config->get('config_timezone')) {
+			date_default_timezone_set($this->config->get('config_timezone'));
+
+			// Sync PHP and DB time zones.
+			$this->db->query("SET time_zone = '" . $this->db->escape(date('P')) . "'");
+		}
+
+		// Encryption
+		$this->registry->set('encryption', new \Encryption($this->config->get('config_encryption')));
+	}
+}
