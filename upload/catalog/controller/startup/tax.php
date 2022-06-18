f58f4df0947414c309a27ebc4ce88d04847165fb
TheCartpenter <scoutch@gmail.com>
2022-06-18 16:57:23 -0400

    Re-added isset lookup in startup/tax controller from catalog.

--- upload/catalog/controller/startup/tax.php
+++ upload/catalog/controller/startup/tax.php
@@ -4,13 +4,13 @@ class ControllerStartupTax extends Controller {
 		$this->registry->set('tax', new \Cart\Tax($this->registry));
 
 		// PHP v7.4+ validation compatibility.
-		if (isset($this->session->data['shipping_address'])) {
+		if (isset($this->session->data['shipping_address']['country_id']) && isset($this->session->data['shipping_address']['zone_id'])) {
 			$this->tax->setShippingAddress((int)$this->session->data['shipping_address']['country_id'], (int)$this->session->data['shipping_address']['zone_id']);
 		} elseif ($this->config->get('config_tax_default') == 'shipping') {
 			$this->tax->setShippingAddress((int)$this->config->get('config_country_id'), (int)$this->config->get('config_zone_id'));
 		}
 
-		if (isset($this->session->data['payment_address'])) {
+		if (isset($this->session->data['payment_address']['country_id']) && isset($this->session->data['payment_address']['zone_id'])) {
 			$this->tax->setPaymentAddress((int)$this->session->data['payment_address']['country_id'], (int)$this->session->data['payment_address']['zone_id']);
 		} elseif ($this->config->get('config_tax_default') == 'payment') {
 			$this->tax->setPaymentAddress((int)$this->config->get('config_country_id'), (int)$this->config->get('config_zone_id'));
