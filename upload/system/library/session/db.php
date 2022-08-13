31d65778f6f1d703391b09035e6c49eedebf3d94
TheCartpenter <scoutch@gmail.com>
2022-08-13 17:00:21 -0400

    Added array cast to json_decode as per MB.

--- upload/system/library/session/db.php
+++ upload/system/library/session/db.php
@@ -16,7 +16,7 @@ class DB {
 		$query = $this->db->query("SELECT `data` FROM `" . DB_PREFIX . "session` WHERE `session_id` = '" . $this->db->escape($session_id) . "' AND `expire` > '" . $this->db->escape(date('Y-m-d H:i:s', time())) . "'");
 
 		if ($query->num_rows) {
-			return json_decode($query->row['data'], true);
+			return (array)json_decode($query->row['data'], true);
 		} else {
 			return array();
 		}
