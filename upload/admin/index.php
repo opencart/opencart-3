4a7220df9d4ec965188f03fd9f66184b7b31dc44
TheCartpenter <scoutch@gmail.com>
2022-10-03 21:58:12 -0400

    Using indent of 4 with admin/index.php file

--- upload/admin/index.php
+++ upload/admin/index.php
@@ -1,16 +1,16 @@
 <?php
 // Version
-define('VERSION', '3.0.3.8');
+define('VERSION', '3.1.1.0');
 
 // Configuration
 if (is_file('config.php')) {
-	require_once('config.php');
+    require_once('config.php');
 }
 
 // Install
 if (!defined('DIR_APPLICATION')) {
-	header('Location: ../install/index.php');
-	exit;
+    header('Location: ../install/index.php');
+    exit;
 }
 
 // Startup
