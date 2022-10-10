be1edee4de3a4e03a03af743ddc322be81f93ab8
TheCartpenter <scoutch@gmail.com>
2022-10-10 14:57:07 -0400

    Added subscription language, TWIG order_shipping and order_invoice

--- upload/admin/language/en-gb/sale/order.php
+++ upload/admin/language/en-gb/sale/order.php
@@ -59,6 +59,7 @@ $_['text_commission_added']      = 'Success: Commission added!';
 $_['text_commission_removed']    = 'Success: Commission removed!';
 $_['text_restock']               = 'Success: Products have been restocked!';
 $_['text_upload']                = 'Your file was successfully uploaded!';
+$_['text_subscription']          = 'Subscription';
 $_['text_picklist']              = 'Dispatch Note';
 
 // Column
