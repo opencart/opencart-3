d4e7f0d1fedd7ae667f293481c323feeaae4edc9
condor2 <condor20_05@yahoo.it>
2022-10-12 10:47:31 +0300

    Fixed view order admin

--- upload/admin/model/sale/order.php
+++ upload/admin/model/sale/order.php
@@ -1,6 +1,6 @@
 <?php
 class ModelSaleOrder extends Model {
-    public function getOrder(int $order_id): int {
+    public function getOrder(int $order_id): array {
         $order_query = $this->db->query("SELECT *, (SELECT CONCAT(c.`firstname`, ' ', c.`lastname`) FROM `" . DB_PREFIX . "customer` c WHERE c.`customer_id` = o.`customer_id`) AS `customer`, (SELECT os.`name` FROM `" . DB_PREFIX . "order_status` os WHERE os.`order_status_id` = o.`order_status_id` AND os.`language_id` = '" . (int)$this->config->get('config_language_id') . "') AS `order_status` FROM `" . DB_PREFIX . "order` o WHERE o.`order_id` = '" . (int)$order_id . "'");
 
         if ($order_query->num_rows) {
