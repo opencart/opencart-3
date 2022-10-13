7cba96a95751017381f61cd021b61ab4bd82c4e4
TheCartpenter <scoutch@gmail.com>
2022-10-13 16:41:18 -0400

    Aligned payment and shipping method models

--- upload/catalog/model/checkout/payment_method.php
+++ upload/catalog/model/checkout/payment_method.php
@@ -1,31 +1,30 @@
 <?php
 class ModelCheckoutPaymentMethod extends Model {
-	public function getMethods(array $payment_address): array {
-        $method_data  = [];
+    public function getMethods(array $payment_address): array {
+        $method_data = [];
 
         $this->load->model('setting/extension');
 
-        $results      = $this->model_setting_extension->getExtensionsByType('payment');
+        $results     = $this->model_setting_extension->getExtensionsByType('payment');
 
         foreach ($results as $result) {
-			if ($this->config->get('payment_' . $result['code'] . '_status')) {
-				$this->load->model('extension/' . $result['extension'] . '/payment/' . $result['code']);
+            if ($this->config->get('payment_' . $result['code'] . '_status')) {
+                $this->load->model('extension/' . $result['extension'] . '/payment/' . $result['code']);
 
-				$payment_method = $this->{'model_extension_' . $result['extension'] . '_payment_' . $result['code']}->getMethod($payment_address);
+                $payment_method = $this->{'model_extension_' . $result['extension'] . '_payment_' . $result['code']}->getMethod($payment_address);
+                if ($payment_method) {
+                    $method_data[$result['code']] = $payment_method;
+                }
+            }
+        }
 
-				if ($payment_method) {
-					$method_data[$result['code']] = $payment_method;
-				}
-			}
-		}
+        $sort_order = [];
 
-		$sort_order = [];
+        foreach ($method_data as $key => $value) {
+            $sort_order[$key] = $value['sort_order'];
+        }
 
-		foreach ($method_data as $key => $value) {
-			$sort_order[$key] = $value['sort_order'];
-		}
-
-		array_multisort($sort_order, SORT_ASC, $method_data);
+        array_multisort($sort_order, SORT_ASC, $method_data);
 
         // Since the API does not use the same session super globals
         // as the one during checkout, we need to validate both.
@@ -37,18 +36,18 @@ class ModelCheckoutPaymentMethod extends Model {
             $customer_id = 0;
         }
 
-		// Stored payment methods
-		$this->load->model('account/payment_method');
+        // Stored payment methods
+        $this->load->model('account/payment_method');
 
-		$payment_methods = $this->model_account_payment_method->getPaymentMethods($customer_id);
+        $payment_methods = $this->model_account_payment_method->getPaymentMethods($customer_id);
 
-		foreach ($payment_methods as $payment_method) {
-			$method_data[$result['code'] . '_' . $result['code']] = [
-				'name' => $payment_method['name'],
-				'code' => $payment_method['code']
-			];
-		}
+        foreach ($payment_methods as $payment_method) {
+            $method_data[$result['code'] . '_' . $result['code']] = [
+                'name' => $payment_method['name'],
+                'code' => $payment_method['code']
+            ];
+        }
 
-		return $method_data;
-	}
+        return $method_data;
+    }
 }
--- upload/catalog/model/checkout/shipping_method.php
+++ upload/catalog/model/checkout/shipping_method.php
@@ -1,37 +1,37 @@
 <?php
 class ModelCheckoutShippingMethod extends Model {
-	public function getMethods(array $shipping_address): array {
+    public function getMethods(array $shipping_address): array {
         $method_data = [];
 
         $this->load->model('setting/extension');
 
         $results     = $this->model_setting_extension->getExtensionsByType('shipping');
 
-		foreach ($results as $result) {
-			if ($this->config->get('shipping_' . $result['code'] . '_status')) {
-				$this->load->model('extension/' . $result['extension'] . '/shipping/' . $result['code']);
+        foreach ($results as $result) {
+            if ($this->config->get('shipping_' . $result['code'] . '_status')) {
+                $this->load->model('extension/' . $result['extension'] . '/shipping/' . $result['code']);
 
-				$quote = $this->{'model_extension_' . $result['extension'] . '_shipping_' . $result['code']}->getQuote($shipping_address);
+                $quote = $this->{'model_extension_' . $result['extension'] . '_shipping_' . $result['code']}->getQuote($shipping_address);
 
-				if ($quote) {
-					$method_data[$result['code']] = [
-						'title'      => $quote['title'],
-						'quote'      => $quote['quote'],
-						'sort_order' => $quote['sort_order'],
-						'error'      => $quote['error']
-					];
-				}
-			}
-		}
+                if ($quote) {
+                    $method_data[$result['code']] = [
+                        'title'      => $quote['title'],
+                        'quote'      => $quote['quote'],
+                        'sort_order' => $quote['sort_order'],
+                        'error'      => $quote['error']
+                    ];
+                }
+            }
+        }
 
-		$sort_order = [];
+        $sort_order = [];
 
-		foreach ($method_data as $key => $value) {
-			$sort_order[$key] = $value['sort_order'];
-		}
+        foreach ($method_data as $key => $value) {
+            $sort_order[$key] = $value['sort_order'];
+        }
 
-		array_multisort($sort_order, SORT_ASC, $method_data);
+        array_multisort($sort_order, SORT_ASC, $method_data);
 
-		return $method_data;
-	}
+        return $method_data;
+    }
 }
