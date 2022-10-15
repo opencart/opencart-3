bf264518fc64caff0864015d4fc29fb759661c70
TheCartpenter <scoutch@gmail.com>
2022-10-15 17:41:55 -0400

    Reversed fraud/ip model

--- upload/admin/model/catalog/subscription_plan.php
+++ upload/admin/model/catalog/subscription_plan.php
@@ -23,7 +23,7 @@ class ModelCatalogSubscriptionPlan extends Model {
     }
 
     public function copySubscriptionPlan(int $subscription_plan_id): void {
-        $data = $this->getSubscriptionPlan($subscription_plan_id);
+        $data                                  = $this->getSubscriptionPlan($subscription_plan_id);
 
         $data['subscription_plan_description'] = $this->getDescription($subscription_plan_id);
 
