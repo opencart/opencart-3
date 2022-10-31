7e8d344a4e51828d98459477db4bb496bd54b64d
TheCartpenter <scoutch@gmail.com>
2022-10-31 12:50:56 -0400

    Improved IF order lookup in sale/subscription controller

--- upload/admin/controller/sale/subscription.php
+++ upload/admin/controller/sale/subscription.php
@@ -665,7 +665,7 @@ class ControllerSaleSubscription extends Controller {
 
             $order_info = $this->model_sale_order->getOrder($subscription_info['order_id']);
 
-            if (!$order_info) {
+            if ((!$order_info) || ($this->request->post['order_id'] == '') || (($this->request->post['order_id'] != $order_info['order_id']) || ($this->request->post['order_id'] != $subscription_info['order_id']))) {
                 $json['error'] = $this->language->get('error_payment_method');
             }
         }
