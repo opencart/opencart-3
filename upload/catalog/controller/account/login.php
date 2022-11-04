4f0174c6bd0e8826ef86db015de161b84d5f77d8
opencartcomvn <82214791+opencartcomvn@users.noreply.github.com>
2022-11-04 13:27:50 +0700

    Update login.php

--- upload/catalog/controller/account/login.php
+++ upload/catalog/controller/account/login.php
@@ -6,43 +6,6 @@ class ControllerAccountLogin extends Controller {
         // Customers
         $this->load->model('account/customer');
 
-        // Login override for admin users
-        if (!empty($this->request->get['token'])) {
-            $this->customer->logout();
-
-            $this->cart->clear();
-
-            unset($this->session->data['order_id']);
-            unset($this->session->data['payment_address']);
-            unset($this->session->data['payment_method']);
-            unset($this->session->data['payment_methods']);
-            unset($this->session->data['shipping_address']);
-            unset($this->session->data['shipping_method']);
-            unset($this->session->data['shipping_methods']);
-            unset($this->session->data['comment']);
-            unset($this->session->data['coupon']);
-            unset($this->session->data['reward']);
-            unset($this->session->data['voucher']);
-            unset($this->session->data['vouchers']);
-
-            $customer_info = $this->model_account_customer->getCustomerByToken($this->request->get['token']);
-
-            if ($customer_info && $this->customer->login($customer_info['email'], '', true)) {
-                // Default Addresses
-                $this->load->model('account/address');
-
-                if ($this->config->get('config_tax_customer') == 'payment') {
-                    $this->session->data['payment_address'] = $this->model_account_address->getAddress($this->customer->getAddressId());
-                }
-
-                if ($this->config->get('config_tax_customer') == 'shipping') {
-                    $this->session->data['shipping_address'] = $this->model_account_address->getAddress($this->customer->getAddressId());
-                }
-
-                $this->response->redirect($this->url->link('account/account', 'customer_token=' . $this->session->data['customer_token'], true));
-            }
-        }
-
         if ($this->customer->isLogged()) {
             $this->response->redirect($this->url->link('account/account', 'customer_token=' . $this->session->data['customer_token'], true));
         }
@@ -158,6 +121,176 @@ class ControllerAccountLogin extends Controller {
         $this->response->setOutput($this->load->view('account/login', $data));
     }
 
+	public function login(): void {
+		$this->load->language('account/login');
+
+		$json = [];
+
+		$this->customer->logout();
+
+		if (!isset($this->request->get['login_token']) || !isset($this->session->data['login_token']) || ($this->request->get['login_token'] != $this->session->data['login_token'])) {
+			$json['redirect'] = $this->url->link('account/login', 'language=' . $this->config->get('config_language'), true);
+		}
+
+		if (!$json) {
+			$keys = [
+				'email',
+				'password',
+				'redirect'
+			];
+
+			foreach ($keys as $key) {
+				if (!isset($this->request->post[$key])) {
+					$this->request->post[$key] = '';
+				}
+			}
+
+			// Check how many login attempts have been made.
+			$this->load->model('account/customer');
+
+			$login_info = $this->model_account_customer->getLoginAttempts($this->request->post['email']);
+
+			if ($login_info && ($login_info['total'] >= $this->config->get('config_login_attempts')) && strtotime('-1 hour') < strtotime($login_info['date_modified'])) {
+				$json['error']['warning'] = $this->language->get('error_attempts');
+			}
+
+			// Check if customer has been approved.
+			$customer_info = $this->model_account_customer->getCustomerByEmail($this->request->post['email']);
+
+			if ($customer_info && !$customer_info['status']) {
+				$json['error']['warning'] = $this->language->get('error_approved');
+			} elseif (!$this->customer->login($this->request->post['email'], html_entity_decode($this->request->post['password'], ENT_QUOTES, 'UTF-8'))) {
+				$json['error']['warning'] = $this->language->get('error_login');
+
+				$this->model_account_customer->addLoginAttempt($this->request->post['email']);
+			}
+		}
+
+		if (!$json) {
+			// Add customer details into session
+			$this->session->data['customer'] = [
+				'customer_id'       => $customer_info['customer_id'],
+				'customer_group_id' => $customer_info['customer_group_id'],
+				'firstname'         => $customer_info['firstname'],
+				'lastname'          => $customer_info['lastname'],
+				'email'             => $customer_info['email'],
+				'telephone'         => $customer_info['telephone'],
+				'custom_field'      => $customer_info['custom_field']
+			];
+
+			unset($this->session->data['shipping_method']);
+			unset($this->session->data['shipping_methods']);
+			unset($this->session->data['payment_method']);
+			unset($this->session->data['payment_methods']);
+
+			// Wishlist
+			if (isset($this->session->data['wishlist']) && is_array($this->session->data['wishlist'])) {
+				$this->load->model('account/wishlist');
+
+				foreach ($this->session->data['wishlist'] as $key => $product_id) {
+					$this->model_account_wishlist->addWishlist($product_id);
+
+					unset($this->session->data['wishlist'][$key]);
+				}
+			}
+
+			// Log the IP info
+			$this->model_account_customer->addLogin($this->customer->getId(), $this->request->server['REMOTE_ADDR']);
+
+			// Create customer token
+			$this->session->data['customer_token'] = oc_token(26);
+
+			$this->model_account_customer->deleteLoginAttempts($this->request->post['email']);
+
+			// Added strpos check to pass McAfee PCI compliance test (http://forum.opencart.com/viewtopic.php?f=10&t=12043&p=151494#p151295)
+			if (isset($this->request->post['redirect']) && (strpos($this->request->post['redirect'], $this->config->get('config_url')) !== false)) {
+				$json['redirect'] = str_replace('&amp;', '&', $this->request->post['redirect']) . '&customer_token=' . $this->session->data['customer_token'];
+			} else {
+				$json['redirect'] = $this->url->link('account/account', '&customer_token=' . $this->session->data['customer_token'], true);
+			}
+		}
+
+		$this->response->addHeader('Content-Type: application/json');
+		$this->response->setOutput(json_encode($json));
+	}
+
+	public function token(): void {
+		$this->load->language('account/login');
+
+		if (isset($this->request->get['email'])) {
+			$email = $this->request->get['email'];
+		} else {
+			$email = '';
+		}
+
+		if (isset($this->request->get['login_token'])) {
+			$token = $this->request->get['login_token'];
+		} else {
+			$token = '';
+		}
+
+		// Login override for admin users
+		$this->customer->logout();
+		$this->cart->clear();
+
+		unset($this->session->data['order_id']);
+		unset($this->session->data['payment_address']);
+		unset($this->session->data['payment_method']);
+		unset($this->session->data['payment_methods']);
+		unset($this->session->data['shipping_address']);
+		unset($this->session->data['shipping_method']);
+		unset($this->session->data['shipping_methods']);
+		unset($this->session->data['comment']);
+		unset($this->session->data['coupon']);
+		unset($this->session->data['reward']);
+		unset($this->session->data['voucher']);
+		unset($this->session->data['vouchers']);
+		unset($this->session->data['customer_token']);
+
+		$this->load->model('account/customer');
+
+		$customer_info = $this->model_account_customer->getCustomerByEmail($email);
+
+		if ($customer_info && $customer_info['token'] && $customer_info['token'] == $token && $this->customer->login($customer_info['email'], '', true)) {
+			// Add customer details into session
+			$this->session->data['customer'] = [
+				'customer_id'       => $customer_info['customer_id'],
+				'customer_group_id' => $customer_info['customer_group_id'],
+				'firstname'         => $customer_info['firstname'],
+				'lastname'          => $customer_info['lastname'],
+				'email'             => $customer_info['email'],
+				'telephone'         => $customer_info['telephone'],
+				'custom_field'      => $customer_info['custom_field']
+			];
+			
+			// Default Addresses
+			$this->load->model('account/address');
+			
+			$address_info = $this->model_account_address->getAddress($this->customer->getAddressId());
+			
+			if ($address_info) {
+				$this->session->data['shipping_address'] = $address_info;
+			}
+
+			if ($this->config->get('config_tax_customer') && $address_info) {
+				$this->session->data[$this->config->get('config_tax_customer') . '_address'] = $address_info;
+			}
+
+			$this->model_account_customer->editToken($email, '');
+
+			// Create customer token
+			$this->session->data['customer_token'] = oc_token(26);
+
+			$this->response->redirect($this->url->link('account/account', '&customer_token=' . $this->session->data['customer_token']));
+		} else {
+			$this->session->data['error'] = $this->language->get('error_login');
+
+			$this->model_account_customer->editToken($email, '');
+
+			$this->response->redirect($this->url->link('account/login'));
+		}
+	}
+	
     protected function validate() {
         // Check how many login attempts have been made.
         $login_info = $this->model_account_customer->getLoginAttempts($this->request->post['email']);
