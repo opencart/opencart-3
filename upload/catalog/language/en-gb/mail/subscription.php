2deaede6463443ed049f5bb37469b21cd7087e00
TheCartpenter <scoutch@gmail.com>
2022-11-06 17:47:29 -0500

    Added subscription text language definition in mail/subscription

--- upload/catalog/language/en-gb/mail/subscription.php
+++ upload/catalog/language/en-gb/mail/subscription.php
@@ -1,15 +1,20 @@
 <?php
+// Text
+$_['text_subscription_trial']    = '%s every %d %s(s) for %d payment(s) then ';
+$_['text_subscription_duration'] = '%s every %d %s(s) for %d payment(s)';
+$_['text_subscription_cancel']   = '%s every %d %s(s) until canceled';
+
 // Error
-$_['error_status']        = 'Warning: The trial subscription status or the subscription status do not match!';
-$_['error_plan']          = 'Warning: The subscription plan does not match!';
-$_['error_order_product'] = 'Warning: The order product ID could not be found!';
-$_['error_order']         = 'Warning: The order ID could not be found!';
-$_['error_customer']      = 'Warning: The customer ID could not be found!';
-$_['error_name']          = 'Warning: The subscription name does not match!';
-$_['error_description']   = 'Warning: The description does not match!';
-$_['error_price']         = 'Warning: The trial price or the price do not match!';
-$_['error_frequency']     = 'Warning: The trial frequency or the frequency do not match!';
-$_['error_cycle']         = 'Warning: The trial cycle or the cycle do not match!';
-$_['error_duration']      = 'Warning: The trial duration or the duration do not match!';
-$_['error_remaining']     = 'Warning: The trial remaining period or the remaining period do not match!';
-$_['error_date_next']     = 'Warning: The next renewal date does not match!';
+$_['error_status']               = 'Warning: The trial subscription status or the subscription status do not match!';
+$_['error_plan']                 = 'Warning: The subscription plan does not match!';
+$_['error_order_product']        = 'Warning: The order product ID could not be found!';
+$_['error_order']                = 'Warning: The order ID could not be found!';
+$_['error_customer']             = 'Warning: The customer ID could not be found!';
+$_['error_name']                 = 'Warning: The subscription name does not match!';
+$_['error_description']          = 'Warning: The description does not match!';
+$_['error_price']                = 'Warning: The trial price or the price do not match!';
+$_['error_frequency']            = 'Warning: The trial frequency or the frequency do not match!';
+$_['error_cycle']                = 'Warning: The trial cycle or the cycle do not match!';
+$_['error_duration']             = 'Warning: The trial duration or the duration do not match!';
+$_['error_remaining']            = 'Warning: The trial remaining period or the remaining period do not match!';
+$_['error_date_next']            = 'Warning: The next renewal date does not match!';
