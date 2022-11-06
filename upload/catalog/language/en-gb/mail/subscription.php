957e4b4bf26059a2aee7b341bc72186e4da9d7fb
TheCartpenter <scoutch@gmail.com>
2022-11-06 17:49:27 -0500

    Added more subscription text language definition in mail/subscription

--- upload/catalog/language/en-gb/mail/subscription.php
+++ upload/catalog/language/en-gb/mail/subscription.php
@@ -3,6 +3,11 @@
 $_['text_subscription_trial']    = '%s every %d %s(s) for %d payment(s) then ';
 $_['text_subscription_duration'] = '%s every %d %s(s) for %d payment(s)';
 $_['text_subscription_cancel']   = '%s every %d %s(s) until canceled';
+$_['text_day']                   = 'day';
+$_['text_week']                  = 'week';
+$_['text_semi_month']            = 'half-month';
+$_['text_month']                 = 'month';
+$_['text_year']                  = 'year';
 
 // Error
 $_['error_status']               = 'Warning: The trial subscription status or the subscription status do not match!';
