1e146252ebb2348985acf125f9006c11d6f158bf
TheCartpenter <scoutch@gmail.com>
2022-11-06 19:24:24 -0500

    Added error_permission language definition to extension/module/subscription

--- upload/catalog/language/en-gb/extension/module/subscription.php
+++ upload/catalog/language/en-gb/extension/module/subscription.php
@@ -1,8 +1,9 @@
 <?php
 // Text
-$_['text_status']     = 'Please check the subscription transactions for the renewal statuses!';
+$_['text_status']      = 'Please check the subscription transactions for the renewal statuses!';
 
 // Error
-$_['error_extension'] = 'Warning: Payment method extension could not be found!';
-$_['error_recurring'] = 'Warning: Payment method does not have recurring payment method!';
-$_['error_payment']   = 'Warning: Payment method %s could not be found!';
+$_['error_permission'] = 'Warning: You do not have permission to access the API!';
+$_['error_extension']  = 'Warning: Payment method extension could not be found!';
+$_['error_recurring']  = 'Warning: Payment method does not have recurring payment method!';
+$_['error_payment']    = 'Warning: Payment method %s could not be found!';
