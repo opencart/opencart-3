eb74db60f869b4aecb5fcb24162eb18ef91cc496
TheCartpenter <scoutch@gmail.com>
2022-11-22 15:33:41 -0500

    Added NO_ZERO_DATE in pgsql library as per MB

--- upload/system/library/db/pgsql.php
+++ upload/system/library/db/pgsql.php
@@ -1,15 +1,24 @@
 <?php
-namespace DB;
+namespace Opencart\System\Library\DB;
 class PgSQL {
     private object $connection;
 
+    /**
+     * Constructor
+     *
+     * @param    string  $hostname
+     * @param    string  $username
+     * @param    string  $password
+     * @param    string  $database
+     * @param    string  $port
+     */
     public function __construct(string $hostname, string $username, string $password, string $database, string $port = '') {
         if (!$port) {
             $port = '5432';
         }
 
         try {
-            $pg = @pg_connect('host=' . $hostname . ' port=' . $port . ' user=' . $username . ' password=' . $password . ' dbname=' . $database . ' options=\'--client_encoding=UTF8\' ');
+            $pg = @pg_connect('host=' . $hostname . ' port=' . $port .  ' user=' . $username . ' password='	. $password . ' dbname=' . $database . ' options=\'--client_encoding=UTF8\' ');
         } catch (\Exception $e) {
             throw new \Exception('Error: Could not make a database link using ' . $username . '@' . $hostname);
         }
@@ -17,9 +26,18 @@ class PgSQL {
         if ($pg) {
             $this->connection = $pg;
             pg_query($this->connection, "SET CLIENT_ENCODING TO 'UTF8'");
+            pg_query("SET SESSION sql_mode = 'NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION'");
+            pg_query($this->connection, "SET FOREIGN_KEY_CHECKS = 0");
         }
     }
 
+    /**
+     * Query
+     *
+     * @param    string  $sql
+     *
+     * @return   bool|object
+     */
     public function query(string $sql): bool|object {
         $resource = pg_query($this->connection, $sql);
 
@@ -37,9 +55,9 @@ class PgSQL {
 
                 pg_free_result($resource);
 
-                $query           = new \stdClass();
-                $query->row      = isset($data[0]) ? $data[0] : [];
-                $query->rows     = $data;
+                $query = new \stdClass();
+                $query->row = isset($data[0]) ? $data[0] : [];
+                $query->rows = $data;
                 $query->num_rows = $i;
 
                 unset($data);
@@ -53,14 +71,42 @@ class PgSQL {
         }
     }
 
-    public function escape(string $value): string {
+    /**
+     * Escape
+     *
+     * @param    string  value
+     *
+     * @return   string
+     */
+    public function escape(string $value): string  {
         return pg_escape_string($this->connection, $value);
     }
 
+    /**
+     * countAffected
+     *
+     * @return   int
+     */
     public function countAffected(): int {
         return pg_affected_rows($this->connection);
     }
 
+    /**
+     * getLastId
+     *
+     * @return   int
+     */
+    public function getLastId(): int {
+        $query = $this->query("SELECT LASTVAL() AS `id`");
+
+        return $query->row['id'];
+    }
+
+    /**
+     * isConnected
+     *
+     * @return   bool
+     */
     public function isConnected(): bool {
         if (pg_connection_status($this->connection) == PGSQL_CONNECTION_OK) {
             return true;
@@ -69,12 +115,12 @@ class PgSQL {
         }
     }
 
-    public function getLastId(): int {
-        $query = $this->query("SELECT LASTVAL() AS `id`");
-
-        return $query->row['id'];
-    }
-
+    /**
+     * Destructor
+     *
+     * Closes the DB connection when this object is destroyed.
+     *
+     */
     public function __destruct() {
         if ($this->connection) {
             pg_close($this->connection);
