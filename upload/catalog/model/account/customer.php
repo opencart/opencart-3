5e897bd1a465f4ae95c1f7c8acf097386534685b
TheCartpenter <scoutch@gmail.com>
2022-11-23 17:52:04 -0500

    Revert "Integrated MB login, token methods as well as the login_token"
    This reverts commit b34da6b3c91f13cd6224936438b14a8ad2de569d.

--- upload/catalog/model/account/customer.php
+++ upload/catalog/model/account/customer.php
@@ -40,10 +40,6 @@ class ModelAccountCustomer extends Model {
         $this->db->query("UPDATE `" . DB_PREFIX . "customer` SET `code` = '" . $this->db->escape($code) . "' WHERE LOWER(`email`) = '" . $this->db->escape(oc_strtolower($email)) . "'");
     }
 
-    public function editToken(string $email, string $token): void {
-        $this->db->query("UPDATE `" . DB_PREFIX . "customer` SET `token` = '" . $this->db->escape($token) . "' WHERE LOWER(`email`) = '" . $this->db->escape(oc_strtolower($email)) . "'");
-    }
-
     public function editNewsletter(string $newsletter): void {
         $this->db->query("UPDATE `" . DB_PREFIX . "customer` SET `newsletter` = '" . (int)$newsletter . "' WHERE `customer_id` = '" . (int)$this->customer->getId() . "'");
     }
