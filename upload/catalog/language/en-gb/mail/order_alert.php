59ec977ad1bc279d1f633c3f704e6a8c09c56b12
TheCartpenter <scoutch@gmail.com>
2022-11-26 10:30:08 -0500

    Added cancel() method in mail/subscription

--- upload/catalog/language/en-gb/mail/order_alert.php
+++ upload/catalog/language/en-gb/mail/order_alert.php
@@ -1,10 +1,11 @@
 <?php
 // Text
-$_['text_subject']      = '%s - Order %s';
-$_['text_received']     = 'You have received an order.';
-$_['text_orders_id']    = 'Order ID:';
-$_['text_date_added']   = 'Date Added:';
-$_['text_order_status'] = 'Order Status:';
-$_['text_product']      = 'Products';
-$_['text_total']        = 'Totals';
-$_['text_comment']      = 'The comments for your order are:';
+$_['text_subject']          = '%s - Order %s';
+$_['text_received']         = 'You have received an order.';
+$_['text_orders_id']        = 'Order ID:';
+$_['text_subscription_id']  = 'Subscription ID:';
+$_['text_date_added']       = 'Date Added:';
+$_['text_order_status']     = 'Order Status:';
+$_['text_product']          = 'Products';
+$_['text_total']            = 'Totals';
+$_['text_comment']          = 'The comments for your order are:';
