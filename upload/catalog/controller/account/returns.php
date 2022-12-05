65f0244cbe4eeb2d9f576e8e7074d8eee9172376
condor2 <condor20_05@yahoo.it>
2022-12-05 16:40:40 +0200

    Fixed pagination return controller

--- upload/catalog/controller/account/returns.php
+++ upload/catalog/controller/account/returns.php
@@ -49,7 +49,7 @@ class ControllerAccountReturns extends Controller {
 
         $return_total = $this->model_account_returns->getTotalReturns();
 
-        $results = $this->model_account_returns->getReturns(($page - 1) * 10, 10);
+        $results = $this->model_account_returns->getReturns(($page - 1) * $this->config->get('theme_' . $this->config->get('config_theme') . '_product_limit'), $this->config->get('theme_' . $this->config->get('config_theme') . '_product_limit'));
 
         foreach ($results as $result) {
             $data['returns'][] = [
