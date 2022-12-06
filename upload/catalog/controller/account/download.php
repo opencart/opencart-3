1b049080b665b1daffc5f191437f75522c4b99ca
TheCartpenter <scoutch@gmail.com>
2022-12-05 19:32:44 -0500

    Revert "Fixed pagination download controller"
    This reverts commit ab85bf9eb5e041358ef5d9fbc3b6eb9e6798aaa4.

--- upload/catalog/controller/account/download.php
+++ upload/catalog/controller/account/download.php
@@ -84,7 +84,7 @@ class ControllerAccountDownload extends Controller {
         $pagination->url = $this->url->link('account/download', 'customer_token=' . $this->session->data['customer_token'] . '&page={page}', true);
 
         $data['pagination'] = $pagination->render();
-        $data['results'] = sprintf($this->language->get('text_pagination'), ($download_total) ? (($page - 1) * $this->config->get('theme_' . $this->config->get('config_theme') . '_product_limit')) + 1 : 0, ((($page - 1) * $this->config->get('theme_' . $this->config->get('config_theme') . '_product_limit')) > ($download_total - $this->config->get('theme_' . $this->config->get('config_theme') . '_product_limit'))) ? $download_total : ((($page - 1) * $this->config->get('theme_' . $this->config->get('config_theme') . '_product_limit')) + $this->config->get('theme_' . $this->config->get('config_theme') . '_product_limit')), $download_total, ceil($download_total / $this->config->get('theme_' . $this->config->get('config_theme') . '_product_limit')));
+        $data['results'] = sprintf($this->language->get('text_pagination'), ($download_total) ? (($page - 1) * 10) + 1 : 0, ((($page - 1) * 10) > ($download_total - 10)) ? $download_total : ((($page - 1) * 10) + 10), $download_total, ceil($download_total / 10));
 
         $data['continue'] = $this->url->link('account/account', 'customer_token=' . $this->session->data['customer_token'], true);
 
