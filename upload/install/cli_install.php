c70af900bc7b0a180e3170632bb6421c9c280cea
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-04-24 08:59:31 -0400

    More PHP 8.2+ requirements.

--- upload/install/cli_install.php
+++ upload/install/cli_install.php
@@ -164,8 +164,8 @@ function install($options) {
 function check_requirements() {
     $error = null;
 
-    if (phpversion() < '8.1') {
-        $error = 'Warning: You need to use PHP8.1+ or above for OpenCart to work!';
+    if (phpversion() < '8.2') {
+        $error = 'Warning: You need to use PHP8.2+ or above for OpenCart to work!';
     }
 
     if (!ini_get('file_uploads')) {
