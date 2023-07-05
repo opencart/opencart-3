ab3c67dc49c8e7372348dbb5fcf53fc9ec8c6d67
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-07-05 15:22:30 -0400

    Update db/pgsql library

--- upload/system/library/db/pgsql.php
+++ upload/system/library/db/pgsql.php
@@ -1,131 +1,128 @@
 <?php
 namespace DB;
 class PgSQL {
-    private object $connection;
+	private object|null $connection;
 
-    /**
-     * Constructor
-     *
-     * @param    string  $hostname
-     * @param    string  $username
-     * @param    string  $password
-     * @param    string  $database
-     * @param    string  $port
-     */
-    public function __construct(string $hostname, string $username, string $password, string $database, string $port = '') {
-        if (!$port) {
-            $port = '5432';
-        }
+	/**
+	 * Constructor
+	 *
+	 * @param    string  $hostname
+	 * @param    string  $username
+	 * @param    string  $password
+	 * @param    string  $database
+	 * @param    string  $port
+	 */
+	public function __construct(string $hostname, string $username, string $password, string $database, string $port = '') {
+		if (!$port) {
+			$port = '5432';
+		}
 
-        try {
-            $pg = @pg_connect('host=' . $hostname . ' port=' . $port . ' user=' . $username . ' password=' . $password . ' dbname=' . $database . ' options=\'--client_encoding=UTF8\' ');
-        } catch (\Exception $e) {
-            throw new \Exception('Error: Could not make a database link using ' . $username . '@' . $hostname);
-        }
+		try {
+			$pg = @pg_connect('host=' . $hostname . ' port=' . $port . ' user=' . $username . ' password=' . $password . ' dbname=' . $database . ' options=\'--client_encoding=UTF8\' ');
+		} catch (\Exception $e) {
+			throw new \Exception('Error: Could not make a database link using ' . $username . '@' . $hostname);
+		}
 
-        if ($pg) {
-            $this->connection = $pg;
-            pg_query($this->connection, "SET CLIENT_ENCODING TO 'UTF8'");
-            pg_query("SET SESSION sql_mode = 'NO_ZERO_IN_DATE,NO_ENGINE_SUBSTITUTION'");
-            pg_query($this->connection, "SET FOREIGN_KEY_CHECKS = 0");
-        }
-    }
+		if ($pg) {
+			$this->connection = $pg;
+			pg_query($this->connection, "SET CLIENT_ENCODING TO 'UTF8'");
 
-    /**
-     * Query
-     *
-     * @param    string  $sql
-     *
-     * @return   bool|object
-     */
-    public function query(string $sql): bool|object {
-        $resource = pg_query($this->connection, $sql);
+			// Sync PHP and DB time zones
+			pg_query($this->connection, "SET TIMEZONE = '" . $this->escape(date('P')) . "'");
+		}
+	}
 
-        if ($resource) {
-            if (is_resource($resource)) {
-                $i = 0;
+	/**
+	 * Query
+	 *
+	 * @param    string  $sql
+	 *
+	 * @return   bool|object
+	 */
+	public function query(string $sql): bool|object {
+		$resource = pg_query($this->connection, $sql);
 
-                $data = [];
+		if ($resource) {
+			if (is_resource($resource)) {
+				$i = 0;
 
-                while ($result = pg_fetch_assoc($resource)) {
-                    $data[$i] = $result;
+				$data = [];
 
-                    $i++;
-                }
+				while ($result = pg_fetch_assoc($resource)) {
+					$data[$i] = $result;
 
-                pg_free_result($resource);
+					$i++;
+				}
 
-                $query = new \stdClass();
-                $query->row = isset($data[0]) ? $data[0] : [];
-                $query->rows = $data;
-                $query->num_rows = $i;
+				pg_free_result($resource);
 
-                unset($data);
+				$query = new \stdClass();
+				$query->row = isset($data[0]) ? $data[0] : [];
+				$query->rows = $data;
+				$query->num_rows = $i;
 
-                return $query;
-            } else {
-                return true;
-            }
-        } else {
-            throw new \Exception('Error: ' . pg_result_error($this->connection) . '<br/>' . $sql);
-        }
-    }
+				unset($data);
 
-    /**
-     * Escape
-     *
-     * @param    string  value
-     *
-     * @return   string
-     */
-    public function escape(string $value): string  {
-        return pg_escape_string($this->connection, $value);
-    }
+				return $query;
+			} else {
+				return true;
+			}
+		} else {
+			throw new \Exception('Error: ' . pg_result_error($resource) . '<br/>' . $sql);
+		}
+	}
+	
+	/**
+	 * Escape
+	 *
+	 * @param    string  value
+	 *
+	 * @return   string
+	 */
+	public function escape(string $value): string  {
+		return pg_escape_string($this->connection, $value);
+	}
 
-    /**
-     * countAffected
-     *
-     * @return   int
-     */
-    public function countAffected(): int {
-        return pg_affected_rows($this->connection);
-    }
+	/**
+	 * countAffected
+	 *
+	 * @return   int
+	 */
+	public function countAffected(): int {
+		return pg_affected_rows($this->connection);
+	}
+	
+	/**
+	 * getLastId
+	 *
+	 * @return   int
+	 */
+	public function getLastId(): int {
+		$query = $this->query("SELECT LASTVAL() AS `id`");
 
-    /**
-     * getLastId
-     *
-     * @return   int
-     */
-    public function getLastId(): int {
-        $query = $this->query("SELECT LASTVAL() AS `id`");
+		return $query->row['id'];
+	}
 
-        return $query->row['id'];
-    }
+	/**
+	 * isConnected
+	 *
+	 * @return   bool
+	 */
+	public function isConnected(): bool {
+		return pg_connection_status($this->connection) == PGSQL_CONNECTION_OK;
+	}
 
-    /**
-     * isConnected
-     *
-     * @return   bool
-     */
-    public function isConnected(): bool {
-        if (pg_connection_status($this->connection) == PGSQL_CONNECTION_OK) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+	/**
+	 * Destructor
+	 *
+	 * Closes the DB connection when this object is destroyed.
+	 *
+	 */
+	public function __destruct() {
+		if ($this->connection) {
+			pg_close($this->connection);
 
-    /**
-     * Destructor
-     *
-     * Closes the DB connection when this object is destroyed.
-     *
-     */
-    public function __destruct() {
-        if ($this->connection) {
-            pg_close($this->connection);
-
-            $this->connection = '';
-        }
-    }
+			$this->connection = null;
+		}
+	}
 }
