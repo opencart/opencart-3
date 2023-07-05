4ab02f4a8245f4280002df13cc1425b299cd475f
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-07-05 15:21:49 -0400

    Update db/pdo library

--- upload/system/library/db/pdo.php
+++ upload/system/library/db/pdo.php
@@ -1,92 +1,133 @@
 <?php
 namespace DB;
 class PDO {
-    private object $connection;
-    private array  $data = [];
-    private int    $affected;
+	private object|null $connection;
+	private array $data = [];
+	private int $affected;
+	
+	/**
+	 * Constructor
+	 *
+	 * @param    string  $hostname
+	 * @param    string  $username
+	 * @param    string  $password
+	 * @param    string  $database
+	 * @param    string  $port
+	 */
+	public function __construct(string $hostname, string $username, string $password, string $database, string $port = '') {
+		if (!$port) {
+			$port = '3306';
+		}
 
-    public function __construct(string $hostname, string $username, string $password, string $database, string $port = '') {
-        if (!$port) {
-            $port = '3306';
-        }
+		try {
+			$pdo = @new \PDO('mysql:host=' . $hostname . ';port=' . $port . ';dbname=' . $database . ';charset=utf8mb4', $username, $password, array(\PDO::ATTR_PERSISTENT => false, \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4 COLLATE utf8mb4_general_ci'));
+		} catch (\PDOException $e) {
+			throw new \Exception('Error: Could not make a database link using ' . $username . '@' . $hostname . '!');
+		}
 
-        try {
-            $pdo = @new \PDO('mysql:host=' . $hostname . ';port=' . $port . ';dbname=' . $database . ';charset=utf8mb4', $username, $password, [\PDO::ATTR_PERSISTENT         => false, \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4 COLLATE utf8mb4_general_ci']);
-        } catch (\PDOException $e) {
-            throw new \Exception('Error: Could not make a database link using ' . $username . '@' . $hostname . '!');
-        }
+		if ($pdo) {
+			$this->connection = $pdo;
 
-        if ($pdo) {
-            $this->connection = $pdo;
-            $this->connection->query("SET SESSION sql_mode = 'NO_ZERO_IN_DATE,NO_ENGINE_SUBSTITUTION'");
-        }
-    }
+			$this->query("SET SESSION sql_mode = 'NO_ZERO_IN_DATE,NO_ENGINE_SUBSTITUTION'");
+			$this->query("SET FOREIGN_KEY_CHECKS = 0");
 
-    public function query(string $sql): bool|object {
-        $sql = preg_replace('/(?:\'\:)([a-z0-9]*.)(?:\')/', ':$1', $sql);
+			// Sync PHP and DB time zones
+			$this->query("SET `time_zone` = '" . $this->escape(date('P')) . "'");
+		}
+	}
+	
+	/**
+	 * Query
+	 *
+	 * @param    string  $sql
+	 *
+	 * @return   bool|object
+	 */
+	public function query(string $sql): bool|object {
+		$sql = preg_replace('/(?:\'\:)([a-z0-9]*.)(?:\')/', ':$1', $sql);
 
-        $statement = $this->connection->prepare($sql);
+		$statement = $this->connection->prepare($sql);
 
-        try {
-            if ($statement && $statement->execute($this->data)) {
-                $this->data = [];
+		try {
+			if ($statement && $statement->execute($this->data)) {
+				$this->data = [];
 
-                if ($statement->columnCount()) {
-                    $data = $statement->fetchAll(\PDO::FETCH_ASSOC);
+				if ($statement->columnCount()) {
+					$data = $statement->fetchAll(\PDO::FETCH_ASSOC);
 
-                    $result           = new \stdClass();
-                    $result->row      = isset($data[0]) ? $data[0] : [];
-                    $result->rows     = $data;
-                    $result->num_rows = count($data);
-                    $this->affected   = 0;
+					$result = new \stdClass();
+					$result->row = isset($data[0]) ? $data[0] : [];
+					$result->rows = $data;
+					$result->num_rows = count($data);
+					$this->affected = 0;
 
-                    return $result;
-                } else {
-                    $this->affected = $statement->rowCount();
+					return $result;
+				} else {
+					$this->affected = $statement->rowCount();
 
-                    return true;
-                }
+					return true;
+				}
 
-                $statement->closeCursor();
-            } else {
-                return true;
-            }
-        } catch (\PDOException $e) {
-            throw new \Exception('Error: ' . $e->getMessage() . ' <br/>Error Code : ' . $e->getCode() . ' <br/>' . $sql);
-        }
+				$statement->closeCursor();
+			} else {
+				return true;
+			}
+		} catch (\PDOException $e) {
+			throw new \Exception('Error: ' . $e->getMessage() . ' <br/>Error Code : ' . $e->getCode() . ' <br/>' . $sql);
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    public function escape(string $value): string {
-        $key = ':' . count($this->data);
+	/**
+	 * Escape
+	 *
+	 * @param    string  value
+	 *
+	 * @return   string
+	 */
+	public function escape(string $value): string {
+		$key = ':' . count($this->data);
 
-        $this->data[$key] = $value;
+		$this->data[$key] = $value;
 
-        return $key;
-    }
+		return $key;
+	}
 
-    public function countAffected(): int {
-        return $this->affected;
-    }
+	/**
+	 * countAffected
+	 *
+	 * @return   int
+	 */
+	public function countAffected(): int {
+		return $this->affected;
+	}
 
-    public function getLastId(): int {
-        return $this->connection->lastInsertId();
-    }
+	/**
+	 * getLastId
+	 *
+	 * @return   int
+	 */
+	public function getLastId(): int {
+		return $this->connection->lastInsertId();
+	}
 
-    public function isConnected(): bool {
-        if ($this->connection) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+	/**
+	 * isConnected
+	 *
+	 * @return   bool
+	 */
+	public function isConnected(): bool {
+		return $this->connection;
+	}
 
-    /**
-     * __destruct
-     * Closes the DB connection when this object is destroyed.
-     */
-    public function __destruct() {
-        unset($this->connection);
-    }
+	/**
+	 * Destructor
+	 *
+	 * Closes the DB connection when this object is destroyed.
+	 *
+	 */
+	public function __destruct() {
+		$this->connection = null;
+	}
 }
