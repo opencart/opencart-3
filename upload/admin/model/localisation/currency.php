87a0cde4dc6c5865d795f78a3dc5942cd695c41a
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-07-08 18:41:04 -0400

    Updated ECB from localisation/currency in admin

--- upload/admin/model/localisation/currency.php
+++ upload/admin/model/localisation/currency.php
@@ -110,57 +110,53 @@ class ModelLocalisationCurrency extends Model {
     public function refresh(): void {
         $curl = curl_init();
 
-        curl_setopt($curl, CURLOPT_URL, 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml');
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($curl, CURLOPT_HEADER, false);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 30);
-        curl_setopt($curl, CURLOPT_TIMEOUT, 30);
+		curl_setopt($curl, CURLOPT_URL, 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml');
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($curl, CURLOPT_HEADER, false);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+		curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 30);
+		curl_setopt($curl, CURLOPT_TIMEOUT, 30);
 
-        $response = curl_exec($curl);
+		$response = curl_exec($curl);
 
-        $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+		$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
-        curl_close($curl);
+		curl_close($curl);
 
-        if ($status == 200) {
-            $dom = new \DOMDocument('1.0', 'UTF-8');
-            $dom->loadXml($response);
+		if ($status == 200) {
+			$dom = new \DOMDocument('1.0', 'UTF-8');
+			$dom->loadXml($response);
 
-            $cube = $dom->getElementsByTagName('Cube')->item(0);
+			$cube = $dom->getElementsByTagName('Cube')->item(0);
 
-            $currencies = [];
-            $currencies['EUR'] = 1.0000;
+			$currencies = [];
 
-            foreach ($cube->getElementsByTagName('Cube') as $currency) {
-                if ($currency->getAttribute('currency')) {
-                    $currencies[$currency->getAttribute('currency')] = $currency->getAttribute('rate');
-                }
-            }
+			$currencies['EUR'] = 1.0000;
 
-            $currency = $this->config->get('config_currency');
+			foreach ($cube->getElementsByTagName('Cube') as $currency) {
+				if ($currency->getAttribute('currency')) {
+					$currencies[$currency->getAttribute('currency')] = $currency->getAttribute('rate');
+				}
+			}
 
-            if ($currencies) {
-                // Currencies
-                $this->load->model('localisation/currency');
+			if ($currencies) {
+                $value = $currencies['EUR'];
+                
+				$this->load->model('localisation/currency');
 
-                $results = $this->getCurrencies();
+				$results = $this->model_localisation_currency->getCurrencies();
 
-                foreach ($results as $result) {
-                    if (isset($currencies[$result['code']])) {
-                        $from = $currencies['EUR'];
+				foreach ($results as $result) {
+					if (isset($currencies[$result['code']])) {
+						$this->editValueByCode($result['code'], 1 / ($value * ($value / $currencies[$result['code']])));
+					}
+				}
 
-                        $to = $currencies[$result['code']];
-
-                        $this->editValueByCode($result['code'], 1 / ($currencies[$currency] * ($from / $to)));
-                    }
-                }
-            }
-
-            $this->editValueByCode($currency, '1.00000');
-
-            $this->cache->delete('currency');
+				$this->editValueByCode('', '1.00000');
+			}
         }
+
+        $this->cache->delete('currency');
     }
 
     public function getTotalCurrencies(): int {
