97f2c300438ebff3f229cc3d5ad212e9c9a7d940
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-07-10 17:23:09 -0400

    Renamed more subscription with subscriptions in model/extension/payment/cheque

--- upload/catalog/model/extension/payment/cheque.php
+++ upload/catalog/model/extension/payment/cheque.php
@@ -5,7 +5,7 @@ class ModelExtensionPaymentCheque extends Model {
 
         $query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "zone_to_geo_zone` WHERE `geo_zone_id` = '" . (int)$this->config->get('payment_cheque_geo_zone_id') . "' AND `country_id` = '" . (int)$address['country_id'] . "' AND (`zone_id` = '" . (int)$address['zone_id'] . "' OR `zone_id` = '0')");
 
-        if ($this->cart->hasSubscription()) {
+        if ($this->cart->hasSubscriptions()) {
             $status = false;
         } elseif (!$this->config->get('payment_cheque_geo_zone_id')) {
             $status = true;
