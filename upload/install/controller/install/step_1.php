7f5391d60b7446dd5a33e6f5cfc34c3c10ec917a
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-09-30 13:40:07 -0400

    Update step_1.php
    Reverted equal spaces.

--- upload/install/controller/install/step_1.php
+++ upload/install/controller/install/step_1.php
@@ -12,14 +12,14 @@ class ControllerInstallStep1 extends Controller {
         $data['heading_title'] = $this->language->get('heading_title');
 
         $data['text_step_1'] = $this->language->get('text_step_1');
-        $data['text_terms']  = $this->language->get('text_terms');
+        $data['text_terms'] = $this->language->get('text_terms');
 
         $data['button_continue'] = $this->language->get('button_continue');
 
         $data['action'] = $this->url->link('install/step_1');
 
-        $data['footer']      = $this->load->controller('common/footer');
-        $data['header']      = $this->load->controller('common/header');
+        $data['footer'] = $this->load->controller('common/footer');
+        $data['header'] = $this->load->controller('common/header');
         $data['column_left'] = $this->load->controller('common/column_left');
 
         $this->response->setOutput($this->load->view('install/step_1', $data));
