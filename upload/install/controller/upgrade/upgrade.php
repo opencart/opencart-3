35d1095cd5709d926286a466c8d33bffd82f6aeb
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-09-30 13:48:52 -0400

    Update upgrade.php
    Reverted equal spaces.

--- upload/install/controller/upgrade/upgrade.php
+++ upload/install/controller/upgrade/upgrade.php
@@ -5,24 +5,24 @@ class ControllerUpgradeUpgrade extends Controller {
 
         $this->document->setTitle($this->language->get('heading_title'));
 
-        $data['heading_title']   = $this->language->get('heading_title');
-        $data['text_upgrade']    = $this->language->get('text_upgrade');
-        $data['text_server']     = $this->language->get('text_server');
-        $data['text_steps']      = $this->language->get('text_steps');
-        $data['text_error']      = $this->language->get('text_error');
-        $data['text_clear']      = $this->language->get('text_clear');
-        $data['text_admin']      = $this->language->get('text_admin');
-        $data['text_user']       = $this->language->get('text_user');
-        $data['text_setting']    = $this->language->get('text_setting');
-        $data['text_store']      = $this->language->get('text_store');
+        $data['heading_title'] = $this->language->get('heading_title');
+        $data['text_upgrade'] = $this->language->get('text_upgrade');
+        $data['text_server'] = $this->language->get('text_server');
+        $data['text_steps'] = $this->language->get('text_steps');
+        $data['text_error'] = $this->language->get('text_error');
+        $data['text_clear'] = $this->language->get('text_clear');
+        $data['text_admin'] = $this->language->get('text_admin');
+        $data['text_user'] = $this->language->get('text_user');
+        $data['text_setting'] = $this->language->get('text_setting');
+        $data['text_store'] = $this->language->get('text_store');
         $data['entry_progress']  = $this->language->get('entry_progress');
         $data['button_continue'] = $this->language->get('button_continue');
-        $data['store']           = HTTP_OPENCART;
-        $data['total']           = count(glob(DIR_APPLICATION . 'model/upgrade/*.php'));
+        $data['store'] = HTTP_OPENCART;
+        $data['total'] = count(glob(DIR_APPLICATION . 'model/upgrade/*.php'));
 
-        $data['header']          = $this->load->controller('common/header');
-        $data['footer']          = $this->load->controller('common/footer');
-        $data['column_left']     = $this->load->controller('common/column_left');
+        $data['header'] = $this->load->controller('common/header');
+        $data['footer'] = $this->load->controller('common/footer');
+        $data['column_left'] = $this->load->controller('common/column_left');
 
         $this->response->setOutput($this->load->view('upgrade/upgrade', $data));
     }
