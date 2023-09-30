2e44f43fa3287ae7f50f6b88962637268e7d4a1e
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-09-30 13:38:08 -0400

    Update header.php
    Reverted equal spaces.

--- upload/install/controller/common/header.php
+++ upload/install/controller/common/header.php
@@ -3,11 +3,11 @@ class ControllerCommonHeader extends Controller {
     public function index() {
         $this->load->language('common/header');
 
-        $data['title']       = $this->document->getTitle();
+        $data['title'] = $this->document->getTitle();
         $data['description'] = $this->document->getDescription();
-        $data['links']       = $this->document->getLinks();
-        $data['styles']      = $this->document->getStyles();
-        $data['scripts']     = $this->document->getScripts();
+        $data['links'] = $this->document->getLinks();
+        $data['styles'] = $this->document->getStyles();
+        $data['scripts'] = $this->document->getScripts();
 
         $data['base'] = HTTP_SERVER;
 
