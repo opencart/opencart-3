e5c7992e0bb44cb27a7e63516375502f1dc7fa79
TheCartpenter <scoutch@gmail.com>
2023-10-18 19:22:42 -0400

    Aligned indent of 8 in catalog/model/extension/payment/paypal

--- upload/catalog/model/extension/payment/paypal.php
+++ upload/catalog/model/extension/payment/paypal.php
@@ -1,86 +1,87 @@
 <?php
+
 class ModelExtensionPaymentPayPal extends Model {
-	public function getMethod(array $address, float $total): array {
-		$method_data = [];
-		
-		$agree_status = $this->getAgreeStatus();
-		
-		if ($this->config->get('payment_paypal_status') && $this->config->get('payment_paypal_client_id') && $this->config->get('payment_paypal_secret') && $agree_status) {
-			$this->load->language('extension/payment/paypal');
-
-			$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "zone_to_geo_zone` WHERE `geo_zone_id` = '" . (int)$this->config->get('payment_paypal_geo_zone_id') . "' AND `country_id` = '" . (int)$address['country_id'] . "' AND (`zone_id` = '" . (int)$address['zone_id'] . "' OR `zone_id` = '0')");
-
-			if (($this->config->get('payment_paypal_total') > 0) && ($this->config->get('payment_paypal_total') > $total)) {
-				$status = false;
-			} elseif (!$this->config->get('payment_paypal_geo_zone_id')) {
-				$status = true;
-			} elseif ($query->num_rows) {
-				$status = true;
-			} else {
-				$status = false;
-			}
-
-			if ($status) {			
-				$method_data = [
-					'code'       => 'paypal',
-					'title'      => $this->language->get('text_paypal_title'),
-					'terms'      => '',
-					'sort_order' => $this->config->get('payment_paypal_sort_order')
-				];
-			}
-		}
-
-		return $method_data;
-	}
-	
-	public function hasProductInCart(int $product_id, array $option = array(), int $recurring_id = 0): int {
-		$query = $this->db->query("SELECT COUNT(*) AS total FROM `" . DB_PREFIX . "cart` WHERE `api_id` = '" . (isset($this->session->data['api_id']) ? (int)$this->session->data['api_id'] : 0) . "' AND `customer_id` = '" . (int)$this->customer->getId() . "' AND `session_id` = '" . $this->db->escape($this->session->getId()) . "' AND `product_id` = '" . (int)$product_id . "' AND `recurring_id` = '" . (int)$recurring_id . "' AND `option` = '" . $this->db->escape(json_encode($option)) . "'");
-				
-		return (int)$query->row['total'];
-	}
-	
-	public function getCountryByCode(string $code): array {
-		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "country` WHERE `iso_code_2` = '" . $this->db->escape($code) . "' AND `status` = '1'");
-				
-		return $query->row;
-	}
-	
-	public function getZoneByCode(int $country_id, string $code): array {
-		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "zone` WHERE `country_id` = '" . (int)$country_id . "' AND (`code` = '" . $this->db->escape($code) . "' OR `name` = '" . $this->db->escape($code) . "') AND `status` = '1'");
-		
-		return $query->row;
-	}
-	
-	public function getAgreeStatus(): bool {
-		$agree_status = true;
-		
-		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "country` WHERE `status` = '1' AND (`iso_code_2` = 'CU' OR `iso_code_2` = 'IR' OR `iso_code_2` = 'SY' OR `iso_code_2` = 'KP')");
-		
-		if ($query->rows) {
-			$agree_status = false;
-		}
-		
-		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "zone` WHERE `country_id` = '220' AND `status` = '1' AND (`code` = '43' OR `code` = '14' OR `code` = '09')");
-		
-		if ($query->rows) {
-			$agree_status = false;
-		}
-		
-		return $agree_status;
-	}
-	
-	public function log(array $data, string $title = null) {
-		// Setting
-		$_config = new \Config();
-		$_config->load('paypal');
-			
-		$config_setting = $_config->get('paypal_setting');
-		
-		$setting = array_replace_recursive((array)$config_setting, (array)$this->config->get('payment_paypal_setting'));
-			
-		if ($setting['general']['debug']) {
-			$log = new Log('paypal.log');
-			$log->write('PayPal debug (' . $title . '): ' . json_encode($data));
-		}
-	}
+        public function getMethod(array $address, float $total): array {
+                $method_data = [];
+                
+                $agree_status = $this->getAgreeStatus();
+                
+                if ($this->config->get('payment_paypal_status') && $this->config->get('payment_paypal_client_id') && $this->config->get('payment_paypal_secret') && $agree_status) {
+                        $this->load->language('extension/payment/paypal');
+                        
+                        $query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "zone_to_geo_zone` WHERE `geo_zone_id` = '" . (int)$this->config->get('payment_paypal_geo_zone_id') . "' AND `country_id` = '" . (int)$address['country_id'] . "' AND (`zone_id` = '" . (int)$address['zone_id'] . "' OR `zone_id` = '0')");
+                        
+                        if (($this->config->get('payment_paypal_total') > 0) && ($this->config->get('payment_paypal_total') > $total)) {
+                                $status = false;
+                        } elseif (!$this->config->get('payment_paypal_geo_zone_id')) {
+                                $status = true;
+                        } elseif ($query->num_rows) {
+                                $status = true;
+                        } else {
+                                $status = false;
+                        }
+                        
+                        if ($status) {
+                                $method_data = [
+                                    'code'       => 'paypal',
+                                    'title'      => $this->language->get('text_paypal_title'),
+                                    'terms'      => '',
+                                    'sort_order' => $this->config->get('payment_paypal_sort_order')
+                                ];
+                        }
+                }
+                
+                return $method_data;
+        }
+        
+        public function hasProductInCart(int $product_id, array $option = [], int $recurring_id = 0): int {
+                $query = $this->db->query("SELECT COUNT(*) AS total FROM `" . DB_PREFIX . "cart` WHERE `api_id` = '" . (isset($this->session->data['api_id']) ? (int)$this->session->data['api_id'] : 0) . "' AND `customer_id` = '" . (int)$this->customer->getId() . "' AND `session_id` = '" . $this->db->escape($this->session->getId()) . "' AND `product_id` = '" . (int)$product_id . "' AND `recurring_id` = '" . (int)$recurring_id . "' AND `option` = '" . $this->db->escape(json_encode($option)) . "'");
+                
+                return (int)$query->row['total'];
+        }
+        
+        public function getCountryByCode(string $code): array {
+                $query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "country` WHERE `iso_code_2` = '" . $this->db->escape($code) . "' AND `status` = '1'");
+                
+                return $query->row;
+        }
+        
+        public function getZoneByCode(int $country_id, string $code): array {
+                $query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "zone` WHERE `country_id` = '" . (int)$country_id . "' AND (`code` = '" . $this->db->escape($code) . "' OR `name` = '" . $this->db->escape($code) . "') AND `status` = '1'");
+                
+                return $query->row;
+        }
+        
+        public function getAgreeStatus(): bool {
+                $agree_status = true;
+                
+                $query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "country` WHERE `status` = '1' AND (`iso_code_2` = 'CU' OR `iso_code_2` = 'IR' OR `iso_code_2` = 'SY' OR `iso_code_2` = 'KP')");
+                
+                if ($query->rows) {
+                        $agree_status = false;
+                }
+                
+                $query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "zone` WHERE `country_id` = '220' AND `status` = '1' AND (`code` = '43' OR `code` = '14' OR `code` = '09')");
+                
+                if ($query->rows) {
+                        $agree_status = false;
+                }
+                
+                return $agree_status;
+        }
+        
+        public function log(array $data, string $title = null) {
+                // Setting
+                $_config = new \Config();
+                $_config->load('paypal');
+                
+                $config_setting = $_config->get('paypal_setting');
+                
+                $setting = array_replace_recursive((array)$config_setting, (array)$this->config->get('payment_paypal_setting'));
+                
+                if ($setting['general']['debug']) {
+                        $log = new Log('paypal.log');
+                        $log->write('PayPal debug (' . $title . '): ' . json_encode($data));
+                }
+        }
 }
