9511ba72db79ade35433880d154feaaa66ef4831
TheCartpenter <scoutch@gmail.com>
2023-10-18 20:08:15 -0400

    Trimmed common controllers in catalog/controller/extension/payment/klarna_checkout

--- upload/catalog/controller/extension/payment/klarna_checkout.php
+++ upload/catalog/controller/extension/payment/klarna_checkout.php
@@ -9,12 +9,12 @@ class ControllerExtensionPaymentKlarnaCheckout extends Controller {
 
 		$this->document->setTitle($this->language->get('heading_title'));
 
-		$data['column_left']    = $this->load->controller('common/column_left');
-		$data['column_right']   = $this->load->controller('common/column_right');
-		$data['content_top']    = $this->load->controller('common/content_top');
+		$data['column_left'] = $this->load->controller('common/column_left');
+		$data['column_right'] = $this->load->controller('common/column_right');
+		$data['content_top'] = $this->load->controller('common/content_top');
 		$data['content_bottom'] = $this->load->controller('common/content_bottom');
-		$data['footer']         = $this->load->controller('common/footer');
-		$data['header']         = $this->load->controller('common/header');
+		$data['footer'] = $this->load->controller('common/footer');
+		$data['header'] = $this->load->controller('common/header');
 
 		$this->response->setOutput($this->load->view('extension/payment/klarna_checkout', $data));
 	}
