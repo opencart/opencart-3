805fcc96f73f7ec291406284d8ca460de6844358
condor2 <condor20_05@yahoo.it>
2023-10-29 11:58:31 +0200

    Changed getSettingValue with getSetting

--- upload/admin/controller/extension/analytics/google.php
+++ upload/admin/controller/extension/analytics/google.php
@@ -55,13 +55,13 @@ class ControllerExtensionAnalyticsGoogle extends Controller {
         if (isset($this->request->post['analytics_google_code'])) {
             $data['analytics_google_code'] = $this->request->post['analytics_google_code'];
         } else {
-            $data['analytics_google_code'] = $this->model_setting_setting->getSettingValue('analytics_google_code', $this->request->get['store_id']);
+            $data['analytics_google_code'] = $this->model_setting_setting->getSetting('analytics_google_code', $this->request->get['store_id']);
         }
 
         if (isset($this->request->post['analytics_google_status'])) {
             $data['analytics_google_status'] = $this->request->post['analytics_google_status'];
         } else {
-            $data['analytics_google_status'] = $this->model_setting_setting->getSettingValue('analytics_google_status', $this->request->get['store_id']);
+            $data['analytics_google_status'] = $this->model_setting_setting->getSetting('analytics_google_status', $this->request->get['store_id']);
         }
 
         $data['header'] = $this->load->controller('common/header');
