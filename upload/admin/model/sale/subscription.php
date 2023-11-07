92a0347bb464ed1fae3eefb1cae52394e2d9b854
TheCartpenter <scoutch@gmail.com>
2023-11-06 19:35:02 -0500

    Fixed bad array push

--- upload/admin/model/sale/subscription.php
+++ upload/admin/model/sale/subscription.php
@@ -188,19 +188,19 @@ class ModelSaleSubscription extends Model {
 		$implode = [];
 
 		if (!empty($data['filter_subscription_id'])) {
-			$implode[] .= "`s`.`subscription_id` = '" . (int)$data['filter_subscription_id'] . "'";
+			$implode[] = "`s`.`subscription_id` = '" . (int)$data['filter_subscription_id'] . "'";
 		}
 
 		if (!empty($data['filter_order_id'])) {
-			$implode[] .= "`s`.`order_id` = '" . (int)$data['filter_order_id'] . "'";
+			$implode[] = "`s`.`order_id` = '" . (int)$data['filter_order_id'] . "'";
 		}
 
 		if (!empty($data['filter_customer'])) {
-			$implode[] .= "LCASE(CONCAT(`o`.`firstname`, ' ', `o`.`lastname`)) LIKE '" . $this->db->escape(oc_strtolower($data['filter_customer']) . '%') . "'";
+			$implode[] = "LCASE(CONCAT(`o`.`firstname`, ' ', `o`.`lastname`)) LIKE '" . $this->db->escape(oc_strtolower($data['filter_customer']) . '%') . "'";
 		}
 
 		if (!empty($data['filter_subscription_status_id'])) {
-			$implode[] .= "`s`.`subscription_status_id` = '" . (int)$data['filter_subscription_status_id'] . "'";
+			$implode[] = "`s`.`subscription_status_id` = '" . (int)$data['filter_subscription_status_id'] . "'";
 		}
 
 		if (!empty($data['filter_date_from'])) {
