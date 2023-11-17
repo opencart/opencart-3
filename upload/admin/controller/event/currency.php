0bebbd44946764d76bed570447c16c21dea24441
TheCartpenter <scoutch@gmail.com>
2023-11-17 18:46:19 -0500

    Added admin currency event

---
+++ upload/admin/controller/event/currency.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Class Currency
+ */
+class ControllerEventCurrency extends Controller {
+	/**
+	 *
+     * admin/model/setting/setting/editSetting
+     * admin/model/localisation/currency/addCurrency
+     * admin/model/localisation/currency/editCurrency
+	 *
+	 * @param string $route
+	 * @param array  $args
+	 * @param mixed  $output
+	 *
+	 * @return void
+	 */
+	 public function index(string &$route, array &$args, mixed &$output) {
+         if ($route == 'model/setting/setting/editSetting' && $args[0] == 'config' && isset($args[1]['config_currency'])) {
+             $this->load->controller('extension/currency/' . $this->config->get('config_currency_engine') . '/currency', $args[1]['config_currency']);
+         } else {
+             $this->load->controller('extension/currency/' . $this->config->get('config_currency_engine') . '/currency', $this->config->get('config_currency'));
+         }
+     }
+}
