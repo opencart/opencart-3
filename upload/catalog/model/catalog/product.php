4fb069ba085148fcc5103c7757ec5190023d93d8
TheCartpenter <scoutch@gmail.com>
2023-11-18 16:35:43 -0500

    Added backtick for p.price in catalog/product model

--- upload/catalog/model/catalog/product.php
+++ upload/catalog/model/catalog/product.php
@@ -169,7 +169,7 @@ class ModelCatalogProduct extends Model {
             if ($data['sort'] == 'pd.name' || $data['sort'] == 'p.model') {
                 $sql .= " ORDER BY LCASE(" . $data['sort'] . ")";
             } elseif ($data['sort'] == 'p.price') {
-                $sql .= " ORDER BY (CASE WHEN special IS NOT NULL THEN special WHEN discount IS NOT NULL THEN discount ELSE `p`.`price` END)";
+                $sql .= " ORDER BY (CASE WHEN `special` IS NOT NULL THEN `special` WHEN `discount` IS NOT NULL THEN `discount` ELSE `p`.`price` END)";
             } else {
                 $sql .= " ORDER BY " . $data['sort'];
             }
