c3ec4596a871e683be618ef20ae4078b85bc959c
condor2 <condor20_05@yahoo.it>
2023-12-25 01:09:11 +0200

    Update laybuy.php
    https://github.com/opencart/opencart/pull/13244#issuecomment-1862572278

--- upload/catalog/model/extension/payment/laybuy.php
+++ upload/catalog/model/extension/payment/laybuy.php
@@ -13,7 +13,7 @@ class ModelExtensionPaymentLaybuy extends Model {
 	 *
 	 * @return void
 	 */
-	public function addTransaction(array $data = [], int $status): void {
+	public function addTransaction($data, int $status): void {
 		$this->log('Report: ' . print_r($data, true), '1');
 		$this->log('Status: ' . $status, '1');
 
