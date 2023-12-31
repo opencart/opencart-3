e6126c3440e0a6b187450e7c41a57c07dacad859
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-12-31 15:35:26 -0500

    Update setting.php

--- upload/catalog/controller/startup/setting.php
+++ upload/catalog/controller/startup/setting.php
@@ -54,8 +54,9 @@ class ControllerStartupSetting extends Controller {
 			$this->db->query("SET `time_zone` = '" . $this->db->escape(date('P')) . "'");
 		}
 
-		// Encryption
-		$encryption = new \Encryption($this->config->get('config_encryption'));
-		$this->registry->set('encryption', $encryption);
+		// Response output compression level
+		if ($this->config->get('config_compression')) {
+			$this->response->setCompression((int)$this->config->get('config_compression'));
+		}
 	}
 }
