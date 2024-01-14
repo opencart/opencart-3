ca9e6f96388bfeb1f04907e1af4844dc4b80c470
TheCartpenter <scoutch@gmail.com>
2024-01-14 12:30:36 -0500

    More tests in cardinity catalog

--- upload/catalog/model/extension/payment/cardinity.php
+++ upload/catalog/model/extension/payment/cardinity.php
@@ -4,8 +4,9 @@
  *
  * @package Catalog\Model\Extension\Payment
  */
-use Cardinity\Exception as CardinityException;
-use Cardinity\Client;
+use \Cardinity\Exception as CardinityException;
+use \Cardinity\Client;
+use \Cardinity\Method\Payment;
 
 class ModelExtensionPaymentCardinity extends Model {
 	/**
