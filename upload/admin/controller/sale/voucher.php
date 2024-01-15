7bb57673ed2eaf4e85402f10f87e91c4c08c3e01
TheCartpenter <scoutch@gmail.com>
2024-01-15 16:17:10 -0500

    voucher.php

--- upload/admin/controller/sale/voucher.php
+++ upload/admin/controller/sale/voucher.php
@@ -661,7 +661,7 @@ class ControllerSaleVoucher extends Controller {
 									'smtp_port'     => $this->config->get('config_mail_smtp_port'),
 									'smtp_timeout'  => $this->config->get('config_mail_smtp_timeout')
 								];
-
+				
 								$mail = new \Mail($this->config->get('config_mail_engine'), $mail_option);
 								$mail->setTo($voucher_info['to_email']);
 								$mail->setFrom($this->config->get('config_email'));
@@ -705,13 +705,6 @@ class ControllerSaleVoucher extends Controller {
 								];
 
 								$mail = new \Mail($this->config->get('config_mail_engine'), $mail_option);								
-								$mail->parameter = $this->config->get('config_mail_parameter');
-								$mail->smtp_hostname = $this->config->get('config_mail_smtp_hostname');
-								$mail->smtp_username = $this->config->get('config_mail_smtp_username');
-								$mail->smtp_password = html_entity_decode($this->config->get('config_mail_smtp_password'), ENT_QUOTES, 'UTF-8');
-								$mail->smtp_port = $this->config->get('config_mail_smtp_port');
-								$mail->smtp_timeout = $this->config->get('config_mail_smtp_timeout');
-
 								$mail->setTo($voucher_info['to_email']);
 								$mail->setFrom($this->config->get('config_email'));
 								$mail->setSender(html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8'));
