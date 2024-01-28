ab566a45184d370ad03713cd97ff9310dddac41c
TheCartpenter <scoutch@gmail.com>
2024-01-27 19:03:34 -0500

    Fixed API model calls from admin orders

--- upload/admin/controller/sale/order.php
+++ upload/admin/controller/sale/order.php
@@ -418,9 +418,9 @@ class ControllerSaleOrder extends Controller {
 
 			$session->start();
 
-			$this->model_user_api->deleteApiSessionBySessionId($session->getId());
+			$this->model_user_api->deleteSessionBySessionId($session->getId());
 
-			$this->model_user_api->addApiSession($api_info['api_id'], $session->getId(), $this->request->server['REMOTE_ADDR']);
+			$this->model_user_api->addSession($api_info['api_id'], $session->getId(), $this->request->server['REMOTE_ADDR']);
 
 			$session->data['api_id'] = $api_info['api_id'];
 
@@ -756,9 +756,9 @@ class ControllerSaleOrder extends Controller {
 
 			$session->start();
 
-			$this->model_user_api->deleteApiSessionBySessionId($session->getId());
+			$this->model_user_api->deleteSessionBySessionId($session->getId());
 
-			$this->model_user_api->addApiSession($api_info['api_id'], $session->getId(), $this->request->server['REMOTE_ADDR']);
+			$this->model_user_api->addSession($api_info['api_id'], $session->getId(), $this->request->server['REMOTE_ADDR']);
 
 			$session->data['api_id'] = $api_info['api_id'];
 
