c3aef13ce9a3ca576cc4923a43adb857a0b91c0e
TheCartpenter <scoutch@gmail.com>
2024-02-03 14:55:23 -0500

    Added protected method for getSetting in storeLoader

--- upload/system/library/googleshopping/traits/storeloader.php
+++ upload/system/library/googleshopping/traits/storeloader.php
@@ -5,8 +5,32 @@ trait StoreLoader {
 	protected function loadStore($store_id): void {
 		$this->registry->set('setting', new \Config());
 
-		foreach ($this->registry->get('load')->model('setting/setting')->model_setting_setting->getSetting('advertise_google', $store_id) as $key => $value) {
+		foreach ($this->getSetting('advertise_google', $store_id) as $key => $value) {
 			$this->registry->get('setting')->set($key, $value);
 		}
+	}
+
+	/**
+	 * Get Setting
+	 *
+	 * @param string $code
+	 * @param int    $store_id
+	 *
+	 * @return array<string, mixed>
+	 */
+	protected function getSetting(string $code, int $store_id = 0): array {
+		$data = [];
+
+		$query = $this->registry->get('db')->query("SELECT * FROM `" . DB_PREFIX . "setting` WHERE `store_id` = '" . (int)$store_id . "' AND `code` = '" . $this->registry->get('db')->escape($code) . "'");
+
+		foreach ($query->rows as $result) {
+			if (!$result['serialized']) {
+				$data[$result['key']] = $result['value'];
+			} else {
+				$data[$result['key']] = json_decode($result['value'], true);
+			}
+		}
+
+		return $data;
 	}
 }
