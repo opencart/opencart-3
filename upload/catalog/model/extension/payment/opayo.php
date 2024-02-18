137420875f2738fc2da834fd63a712aba23cb057
TheCartpenter <scoutch@gmail.com>
2024-02-18 00:04:02 -0500

    Removed last $total from opayo

--- upload/catalog/model/extension/payment/opayo.php
+++ upload/catalog/model/extension/payment/opayo.php
@@ -17,9 +17,7 @@ class ModelExtensionPaymentOpayo extends Model {
 
 		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "zone_to_geo_zone` WHERE `geo_zone_id` = '" . (int)$this->config->get('payment_opayo_geo_zone_id') . "' AND `country_id` = '" . (int)$address['country_id'] . "' AND (`zone_id` = '" . (int)$address['zone_id'] . "' OR `zone_id` = '0')");
 
-		if ($this->config->get('payment_opayo_total') > 0 && $this->config->get('payment_opayo_total') > $total) {
-			$status = false;
-		} elseif (!$this->config->get('payment_opayo_geo_zone_id')) {
+		if (!$this->config->get('payment_opayo_geo_zone_id')) {
 			$status = true;
 		} elseif ($query->num_rows) {
 			$status = true;
