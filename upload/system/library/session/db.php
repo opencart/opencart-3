30aee727c949836eceb73c38ac700931eb9a7594
TheCartpenter <scoutch@gmail.com>
2024-03-23 10:28:48 -0400

    Modified DB Engine in session/db class

--- upload/system/library/session/db.php
+++ upload/system/library/session/db.php
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS `session` (
   `data` text NOT NULL,
   `expire` datetime NOT NULL,
   PRIMARY KEY (`session_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 */
 namespace Session;
 /**
