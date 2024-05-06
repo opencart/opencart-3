2bb9114b67606a7b2c735c2aed87022b4b58e02e
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2024-05-05 20:57:40 -0400

    Update cart.php

--- upload/system/library/cart/cart.php
+++ upload/system/library/cart/cart.php
@@ -27,7 +27,7 @@ class Cart {
 
 		if ($this->customer->getId()) {
 			// We want to change the session ID on all the old items in the customers cart
-			$this->db->query("UPDATE `" . DB_PREFIX . "cart` SET `session_id` = '" . $this->db->escape($this->session->getId()) . "', `date_added` = NOW() WHERE `api_id` = '0' AND `customer_id` = '" . (int)$this->customer->getId() . "'");
+			$this->db->query("UPDATE `" . DB_PREFIX . "cart` SET `session_id` = '" . $this->db->escape($this->session->getId()) . "' WHERE `api_id` = '0' AND `customer_id` = '" . (int)$this->customer->getId() . "'");
 
 			// Once the customer is logged in we want to update the customers cart
 			$cart_query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "cart` WHERE `api_id` = '0' AND `customer_id` = '0' AND `session_id` = '" . $this->db->escape($this->session->getId()) . "'");
