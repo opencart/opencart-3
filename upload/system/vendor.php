568cb80e3c0fec6319c7753abf5799a0f2db856c
TheCartpenter <scoutch@gmail.com>
2024-05-11 12:29:12 -0400

    Added validation on Ctype and Mbstring vendors

--- upload/system/vendor.php
+++ upload/system/vendor.php
@@ -42,11 +42,15 @@ if (is_file(DIR_STORAGE . 'vendor/symfony/deprecation-contracts/function.php')) 
 
 // symfony/polyfill-ctype
 $autoloader->register('Symfony\Polyfill\Ctype', DIR_STORAGE . 'vendor/symfony/polyfill-ctype//', true);
-require_once(DIR_STORAGE . 'vendor/symfony/polyfill-ctype/bootstrap.php');
+if (is_file(DIR_STORAGE . 'vendor/symfony/polyfill-ctype/bootstrap.php')) {
+	require_once(DIR_STORAGE . 'vendor/symfony/polyfill-ctype/bootstrap.php');
+}
 
 // symfony/polyfill-mbstring
 $autoloader->register('Symfony\Polyfill\Mbstring', DIR_STORAGE . 'vendor/symfony/polyfill-mbstring//', true);
-require_once(DIR_STORAGE . 'vendor/symfony/polyfill-mbstring/bootstrap.php');
+if (is_file(DIR_STORAGE . 'vendor/symfony/polyfill-mbstring/bootstrap.php')) {
+	require_once(DIR_STORAGE . 'vendor/symfony/polyfill-mbstring/bootstrap.php');
+}
 
 // twig/twig
 $autoloader->register('Twig', DIR_STORAGE . 'vendor/twig/twig/src/', true);
