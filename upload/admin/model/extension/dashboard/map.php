811fc05153442bb9c40446e57539d2149509094c
TheCartpenter <scoutch@gmail.com>
2024-05-19 13:26:11 -0400

    Switched from LEFT to RIGHT JOIN in extension/dashboard/map model

--- upload/admin/model/extension/dashboard/map.php
+++ upload/admin/model/extension/dashboard/map.php
@@ -13,7 +13,7 @@ class ModelExtensionDashboardMap extends Model {
 		}
 
 		if ($implode) {
-			$query = $this->db->query("SELECT COUNT(*) AS `total`, SUM(`o`.`total`) AS `amount`, LCASE(`c`.`iso_code_2`) AS `iso_code_2` FROM `" . DB_PREFIX . "order` `o` LEFT JOIN `" . DB_PREFIX . "country` `c` ON (`o`.`payment_country_id` = `c`.`country_id`) WHERE `o`.`order_status_id` IN(" . implode(',', $implode) . ") GROUP BY `o`.`payment_country_id`");
+			$query = $this->db->query("SELECT COUNT(*) AS `total`, SUM(`o`.`total`) AS `amount`, LCASE(`c`.`iso_code_2`) AS `iso_code_2` FROM `" . DB_PREFIX . "order` `o` RIGHT JOIN `" . DB_PREFIX . "country` `c` ON (`o`.`payment_country_id` = `c`.`country_id`) WHERE (" . implode(" OR ", $implode) . ") GROUP BY `o`.`payment_country_id`");
 
 			return $query->rows;
 		} else {
