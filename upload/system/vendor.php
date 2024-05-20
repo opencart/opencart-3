04c039b88b3ced573bd0e24970687e8a7d9ac55f
TheCartpenter <scoutch@gmail.com>
2024-05-20 19:29:48 -0400

    Added resources path in system/vendor as per MB

--- upload/system/vendor.php
+++ upload/system/vendor.php
@@ -60,3 +60,15 @@ if (is_file(DIR_STORAGE . 'vendor/symfony/polyfill-php83/bootstrap.php')) {
 
 // twig/twig
 $autoloader->register('Twig', DIR_STORAGE . 'vendor/twig/twig/src/', true);
+if (is_file(DIR_STORAGE . 'vendor/twig/twig/src/Resources/core.php')) {
+	require_once(DIR_STORAGE . 'vendor/twig/twig/src/Resources/core.php');
+}
+if (is_file(DIR_STORAGE . 'vendor/twig/twig/src/Resources/debug.php')) {
+	require_once(DIR_STORAGE . 'vendor/twig/twig/src/Resources/debug.php');
+}
+if (is_file(DIR_STORAGE . 'vendor/twig/twig/src/Resources/escaper.php')) {
+	require_once(DIR_STORAGE . 'vendor/twig/twig/src/Resources/escaper.php');
+}
+if (is_file(DIR_STORAGE . 'vendor/twig/twig/src/Resources/string_loader.php')) {
+	require_once(DIR_STORAGE . 'vendor/twig/twig/src/Resources/string_loader.php');
+}
