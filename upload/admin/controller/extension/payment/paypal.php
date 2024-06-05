9672ab57a97a1ba97a3064f173a1ae1d3164569d
TheCartpenter <scoutch@gmail.com>
2024-06-04 21:24:43 -0400

    Added space on load language extension in PayPal Checkout

--- upload/admin/controller/extension/payment/paypal.php
+++ upload/admin/controller/extension/payment/paypal.php
@@ -2109,7 +2109,7 @@ class ControllerExtensionPaymentPayPal extends Controller {
 	
 	public function order_info_before($route, &$data) {
 		if ($this->config->get('payment_paypal_status') && !empty($this->request->get['order_id'])) {
-			$this->load->language('extension/payment/paypal','extension_payment_paypal');
+			$this->load->language('extension/payment/paypal', 'extension_payment_paypal');
 
 			$content = $this->getPaymentDetails((int)$this->request->get['order_id']);
 			
